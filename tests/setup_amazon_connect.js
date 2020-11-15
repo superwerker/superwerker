0fac4e549052c183ab77a5de5279eadd33d70e2d
Soenke Ruempler <soenke.ruempler@superluminar.io>
2020-11-15 15:34:38 +0100

    durchstich connect setup

--- tests/setup_amazon_connect.js
+++ tests/setup_amazon_connect.js
@@ -1,11 +1,16 @@
 var synthetics = require('Synthetics');
 const AWS = require("aws-sdk");
+
 const LOG = require('SyntheticsLogger');
 const url = require('url');
 const fs = require('fs');
 
+const CONNECT_SSM_PARAMETER = '/superwerker/tests/connect'
+const ACCOUNTID = '215867065708'
 const AWS_CONNECT_REGION = 'us-east-1'
-const domain = 'schnuffipuffi2'
+AWS.config.update({region: AWS_CONNECT_REGION});
+
+const domain = 'superwerker-tests'
 
 
 const httpGet = url => {
@@ -55,10 +60,10 @@ const flowBuilderBlueprint = async function () {
         // await createinstance(page, {
         //     'Domain': domain
         // });
-        // await page.waitFor(5000);
-        await open(page, {
+        const instanceId =  await open(page, {
             'Domain': domain
         });
+        console.log(instanceId)
         let hostx = new url.URL(await page.url()).host;
         while (hostx.indexOf('awsapps') == -1) {
             await page.waitFor(20000);
@@ -70,9 +75,14 @@ const flowBuilderBlueprint = async function () {
         let prompts = await uploadprompts(page, {
             'Domain': domain
         });
-        await createflow(page, {
-            'Domain': domain
-        }, prompts);
+        await createflow(
+            page,
+            instanceId, {
+               'Domain': domain
+            },
+            prompts
+        );
+
         let number = await claimnumber(page, {
             'Domain': domain
         });
@@ -85,8 +95,9 @@ const flowBuilderBlueprint = async function () {
         });
         variables['PHONE_NUMBER'] = number['PhoneNumber'].replace(/[ -]/g, "")
 
+        const ssm = new AWS.SSM();
         await ssm.putParameter({
-            Name: process.env.CONNECT_SSM_PARAMETER,
+            Name: CONNECT_SSM_PARAMETER,
             Type: "String",
             Value: JSON.stringify(variables),
             Overwrite: true
@@ -108,14 +119,20 @@ async function createinstance(page, properties) {
         timeout: 0,
         waitUntil: ['domcontentloaded']
     });
-    await page.waitFor(5000);
 
     let directory = await page.$('input[ng-model="ad.directoryAlias"]');
     await directory.press('Backspace');
     await directory.type(properties.Domain, { delay: 100 });
 
-    page.focus('button.awsui-button-variant-primary');
-    await page.click('button.awsui-button-variant-primary');
+    // await page.waitFor(2000);
+    // const submit_button = await  page.$('#c > div > div > div > div.onboarding-main-pane > div > div.onboarding-main-pane-footer > span:nth-child(3) > awsui-button > button > span');
+    // submit_button.scrollIntoView();
+
+    // somehow it seems we need to scroll to the button to click it
+    page.$eval('#c > div > div > div > div.onboarding-main-pane > div > div.onboarding-main-pane-footer > span:nth-child(3) > awsui-button > button > span', (el) => el.scrollIntoView())
+
+
+    await page.click('#c > div > div > div > div.onboarding-main-pane > div > div.onboarding-main-pane-footer > span:nth-child(3) > awsui-button > button > span');
 
     await page.waitForSelector('label.vertical-padding.option-label');
     await page.waitFor(200);
@@ -152,9 +169,11 @@ async function open(page, properties) {
         timeout: 0,
         waitUntil: ['domcontentloaded']
     });
-    await page.waitFor(8000);
-
     await page.waitFor(3000);
+    const instanceId = (await page.evaluate(
+        anchor => anchor.getAttribute('href'),
+        await page.$('table > tbody > tr > td:nth-child(1) > div > a')
+    )).split('/')[1];
 
     await page.click('table > tbody > tr > td:nth-child(1) > div > a');
 
@@ -172,7 +191,7 @@ async function open(page, properties) {
 
     await page.waitFor(8000);
 
-    
+    return instanceId
 }
 
 async function claimnumber(page, properties) {
@@ -302,7 +321,10 @@ async function uploadprompts(page, properties) {
         await page.waitFor(1000);
 
         let input1 = await page.$('#name');
-        await input1.press('Backspace');
+        await input1.click({ clickCount: 3 }) // select everything to overwrite it by typing
+
+
+        await page.waitFor(1000);
         await input1.type(filename, { delay: 100 });
 
         await page.waitFor(1000);
@@ -325,282 +347,185 @@ async function uploadprompts(page, properties) {
     return ret;
 }
 
-async function createflow(page, properties, prompts) {
-    let host = 'https://' + new url.URL(await page.url()).host;
-
-    do {
-        await page.goto(host + "/connect/contact-flows/create?type=contactFlow", {
-            timeout: 0,
-            waitUntil: ['domcontentloaded']
-        });
-        await page.waitFor(5000);
-        LOG.info("Checking for correct load");
-        LOG.debug(host + "/connect/contact-flows/create?type=contactFlow");
-    } while (await page.$('#angularContainer') === null);
-
-    
-
-    await page.click('#can-edit-contact-flow > div > awsui-button > button');
-
-    await page.waitFor(200);
-
-    
-
-    await page.click('li[ng-if="cfImportExport"]');
-
-    await page.waitFor(500);
-
-    await page.setBypassCSP(true);
-
-    
+async function createflow(page, instanceId, properties, prompts) {
 
     let flow = `{
-    "modules": [
-        {
-            "id": "a238d7ff-9df4-481b-bcf5-e472c3a51abf",
-            "type": "PlayPrompt",
-            "branches": [
-                {
-                    "condition": "Success",
-                    "transition": "39ca9b44-c416-45eb-b2c0-591956bd2fe9"
-                }
-            ],
-            "parameters": [
-                {
-                    "name": "AudioPrompt",
-                    "value": "prompt2",
-                    "namespace": "External",
-                    "resourceName": null
-                }
-            ],
-            "metadata": {
-                "position": {
-                    "x": 700,
-                    "y": 16
-                },
-                "useDynamic": true
-            }
+  "Version": "2019-10-30",
+  "StartAction": "e30d63b7-e7d5-42df-9dea-f93e0bed321d",
+  "Metadata": {
+    "entryPointPosition": {
+      "x": 24,
+      "y": 17
+    },
+    "snapToGrid": false,
+    "ActionMetadata": {
+      "a238d7ff-9df4-481b-bcf5-e472c3a51abf": {
+        "position": {
+          "x": 700,
+          "y": 16
         },
-        {
-            "id": "1f4d3616-77cc-4cef-8881-949c531e13ce",
-            "type": "PlayPrompt",
-            "branches": [
-                {
-                    "condition": "Success",
-                    "transition": "a238d7ff-9df4-481b-bcf5-e472c3a51abf"
-                }
-            ],
-            "parameters": [
-                {
-                    "name": "AudioPrompt",
-                    "value": "prompt1",
-                    "namespace": "External",
-                    "resourceName": null
-                }
-            ],
-            "metadata": {
-                "position": {
-                    "x": 456,
-                    "y": 19
-                },
-                "useDynamic": true
-            }
+        "useDynamic": true
+      },
+      "1f4d3616-77cc-4cef-8881-949c531e13ce": {
+        "position": {
+          "x": 456,
+          "y": 19
         },
-        {
-            "id": "ad3b6726-dfed-40fe-b4c7-95a9751fc4a7",
-            "type": "InvokeExternalResource",
-            "branches": [
-                {
-                    "condition": "Success",
-                    "transition": "1f4d3616-77cc-4cef-8881-949c531e13ce"
-                },
-                {
-                    "condition": "Error",
-                    "transition": "f5205242-eeb0-4b71-bb47-f8c2adf848fa"
-                }
-            ],
-            "parameters": [
-                {
-                    "name": "FunctionArn",
-                    "value": "arn:aws:lambda:us-east-1:${ACCOUNTID}:function:AccountAutomator",
-                    "namespace": null
-                },
-                {
-                    "name": "TimeLimit",
-                    "value": "8"
-                }
-            ],
-            "metadata": {
-                "position": {
-                    "x": 191,
-                    "y": 15
-                },
-                "dynamicMetadata": {},
-                "useDynamic": false
-            },
-            "target": "Lambda"
+        "useDynamic": true
+      },
+      "39ca9b44-c416-45eb-b2c0-591956bd2fe9": {
+        "position": {
+          "x": 948,
+          "y": 18
         },
-        {
-            "id": "39ca9b44-c416-45eb-b2c0-591956bd2fe9",
-            "type": "PlayPrompt",
-            "branches": [
-                {
-                    "condition": "Success",
-                    "transition": "406812d0-65de-4f5a-ba33-89c450b94238"
-                }
-            ],
-            "parameters": [
-                {
-                    "name": "AudioPrompt",
-                    "value": "prompt3",
-                    "namespace": "External",
-                    "resourceName": null
-                }
-            ],
-            "metadata": {
-                "position": {
-                    "x": 948,
-                    "y": 18
-                },
-                "useDynamic": true
-            }
+        "useDynamic": true
+      },
+      "ad3b6726-dfed-40fe-b4c7-95a9751fc4a7": {
+        "position": {
+          "x": 191,
+          "y": 15
         },
-        {
-            "id": "f5205242-eeb0-4b71-bb47-f8c2adf848fa",
-            "type": "Disconnect",
-            "branches": [],
-            "parameters": [],
-            "metadata": {
-                "position": {
-                    "x": 1442,
-                    "y": 22
-                }
-            }
+        "dynamicMetadata": {},
+        "useDynamic": false
+      },
+      "e30d63b7-e7d5-42df-9dea-f93e0bed321d": {
+        "position": {
+          "x": 120,
+          "y": 242
         },
-        {
-            "id": "406812d0-65de-4f5a-ba33-89c450b94238",
-            "type": "PlayPrompt",
-            "branches": [
-                {
-                    "condition": "Success",
-                    "transition": "2298a0bd-cb66-4476-b1cb-1680a079eca6"
-                }
-            ],
-            "parameters": [
-                {
-                    "name": "AudioPrompt",
-                    "value": "prompt4",
-                    "namespace": "External",
-                    "resourceName": null
-                }
-            ],
-            "metadata": {
-                "position": {
-                    "x": 1198,
-                    "y": 17
-                },
-                "useDynamic": true
-            }
-        },
-        {
-            "id": "2298a0bd-cb66-4476-b1cb-1680a079eca6",
-            "type": "PlayPrompt",
-            "branches": [
-                {
-                    "condition": "Success",
-                    "transition": "f5205242-eeb0-4b71-bb47-f8c2adf848fa"
-                }
-            ],
-            "parameters": [
-                {
-                    "name": "AudioPrompt",
-                    "value": "${prompts['a-10-second-silence.wav']}",
-                    "namespace": null,
-                    "resourceName": "a-10-second-silence.wav"
-                }
-            ],
-            "metadata": {
-                "position": {
-                    "x": 1395,
-                    "y": 268
-                },
-                "useDynamic": false,
-                "promptName": "a-10-second-silence.wav"
-            }
-        },
-        {
-            "id": "e30d63b7-e7d5-42df-9dea-f93e0bed321d",
-            "type": "PlayPrompt",
-            "branches": [
-                {
-                    "condition": "Success",
-                    "transition": "ad3b6726-dfed-40fe-b4c7-95a9751fc4a7"
-                }
-            ],
-            "parameters": [
-                {
-                    "name": "AudioPrompt",
-                    "value": "${prompts['a-10-second-silence.wav']}",
-                    "namespace": null,
-                    "resourceName": "a-10-second-silence.wav"
-                }
-            ],
-            "metadata": {
-                "position": {
-                    "x": 120,
-                    "y": 242
-                },
-                "useDynamic": false,
-                "promptName": "a-10-second-silence.wav"
-            }
+        "useDynamic": false,
+        "promptName": "a-10-second-silence.waa-10-second-silence.wav"
+      },
+      "f5205242-eeb0-4b71-bb47-f8c2adf848fa": {
+        "position": {
+          "x": 1442,
+          "y": 22
         }
-    ],
-    "version": "1",
-    "type": "contactFlow",
-    "start": "e30d63b7-e7d5-42df-9dea-f93e0bed321d",
-    "metadata": {
-        "entryPointPosition": {
-            "x": 24,
-            "y": 17
+      },
+      "406812d0-65de-4f5a-ba33-89c450b94238": {
+        "position": {
+          "x": 1198,
+          "y": 17
         },
-        "snapToGrid": false,
-        "name": "myFlow",
-        "description": "An example flowgit
-        "type": "contactFlow",
-        "status": "published",
-        "hash": "f8c17f9cd5523dc9c62111e55d2c225e0ee90ad8d509d677429cf6f7f2497a2f"
+        "useDynamic": true
+      },
+      "2298a0bd-cb66-4476-b1cb-1680a079eca6": {
+        "position": {
+          "x": 1395,
+          "y": 268
+        },
+        "useDynamic": false,
+        "promptName": "a-10-second-silence.waa-10-second-silence.wav"
+      }
     }
-}`;
+  },
+  "Actions": [
+    {
+      "Identifier": "a238d7ff-9df4-481b-bcf5-e472c3a51abf",
+      "Parameters": {
+        "PromptId": "$.External.prompt2"
+      },
+      "Transitions": {
+        "NextAction": "39ca9b44-c416-45eb-b2c0-591956bd2fe9",
+        "Errors": [],
+        "Conditions": []
+      },
+      "Type": "MessageParticipant"
+    },
+    {
+      "Identifier": "1f4d3616-77cc-4cef-8881-949c531e13ce",
+      "Parameters": {
+        "PromptId": "$.External.prompt1"
+      },
+      "Transitions": {
+        "NextAction": "a238d7ff-9df4-481b-bcf5-e472c3a51abf",
+        "Errors": [],
+        "Conditions": []
+      },
+      "Type": "MessageParticipant"
+    },
+    {
+      "Identifier": "39ca9b44-c416-45eb-b2c0-591956bd2fe9",
+      "Parameters": {
+        "PromptId": "$.External.prompt3"
+      },
+      "Transitions": {
+        "NextAction": "406812d0-65de-4f5a-ba33-89c450b94238",
+        "Errors": [],
+        "Conditions": []
+      },
+      "Type": "MessageParticipant"
+    },
+    {
+      "Identifier": "ad3b6726-dfed-40fe-b4c7-95a9751fc4a7",
+      "Parameters": {
+        "LambdaFunctionARN": "arn:aws:lambda:us-east-1:${ACCOUNTID}:function:AccountAutomator",
+        "InvocationTimeLimitSeconds": "8"
+      },
+      "Transitions": {
+        "NextAction": "1f4d3616-77cc-4cef-8881-949c531e13ce",
+        "Errors": [
+          {
+            "NextAction": "f5205242-eeb0-4b71-bb47-f8c2adf848fa",
+            "ErrorType": "NoMatchingError"
+          }
+        ],
+        "Conditions": []
+      },
+      "Type": "InvokeLambdaFunction"
+    },
+    {
+      "Identifier": "e30d63b7-e7d5-42df-9dea-f93e0bed321d",
+      "Parameters": {
+        "PromptId": "${prompts['a-10-second-silence.wav']}"
+      },
+      "Transitions": {
+        "NextAction": "ad3b6726-dfed-40fe-b4c7-95a9751fc4a7",
+        "Errors": [],
+        "Conditions": []
+      },
+      "Type": "MessageParticipant"
+    },
+    {
+      "Identifier": "f5205242-eeb0-4b71-bb47-f8c2adf848fa",
+      "Type": "DisconnectParticipant",
+      "Parameters": {},
+      "Transitions": {}
+    },
+    {
+      "Identifier": "406812d0-65de-4f5a-ba33-89c450b94238",
+      "Parameters": {
+        "PromptId": "$.External.prompt4"
+      },
+      "Transitions": {
+        "NextAction": "2298a0bd-cb66-4476-b1cb-1680a079eca6",
+        "Errors": [],
+        "Conditions": []
+      },
+      "Type": "MessageParticipant"
+    },
+    {
+      "Identifier": "2298a0bd-cb66-4476-b1cb-1680a079eca6",
+      "Parameters": {
+        "PromptId": "${prompts['a-10-second-silence.wav']}"
+      },
+      "Transitions": {
+        "NextAction": "f5205242-eeb0-4b71-bb47-f8c2adf848fa",
+        "Errors": [],
+        "Conditions": []
+      },
+      "Type": "MessageParticipant"
+    }
+  ]
+}
+`;
+    console.log(instanceId)
+    const connect = new AWS.Connect();
+    await connect.createContactFlow({
+        InstanceId:  instanceId,
+        Content: flow,
+        Type: 'CONTACT_FLOW',
+        Name: 'myFlow',
+    }).promise()
 
-    /*fs.writeFileSync("/tmp/flow.json", flow, {
-        mode: 0o777
-    });*/
-
-    LOG.debug(flow);
-
-    await page.waitFor(5000);
-
-    page.click('#import-cf-file-button');
-    let fileinput = await page.$('#import-cf-file');
-    LOG.debug(fileinput);
-    await page.waitFor(1000);
-    
-    //await fileinput.uploadFile('/tmp/flow.json'); // broken!
-
-    await page.evaluate((flow) => {
-        angular.element(document.getElementById('import-cf-file')).scope().importContactFlow(new Blob([flow], {type: "application/json"}));
-    }, flow);
-
-    await page.waitFor(5000);
-
-    
-
-    await page.click('.header-button'); // Publish
-    await page.waitFor(2000);
-
-    await page.click('awsui-button[text="Publish"] > button'); // Publish modal
-
-    await page.waitFor(8000);
-
-    
 }
