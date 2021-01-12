55cce01857a9ee9731e0f3b390b8e685d6af541f
Soenke Ruempler <soenke.ruempler@superluminar.io>
2021-01-12 10:54:41 +0100

    remove obsolete NAMED_IAM capability
    since no IAM roles are created with fixed names

--- tests/setup-test-env.sh
+++ tests/setup-test-env.sh
@@ -34,7 +34,7 @@ aws configure --profile test_account_${aws_account_id} set source_profile ${SOUR
 aws sts get-caller-identity --profile test_account_${aws_account_id} --no-cli-pager
 
 # setup superwerker in vended account
-aws --profile test_account_${aws_account_id} cloudformation deploy --stack-name superwerker --template-file components/superwerker.yaml --parameter-overrides Domain=${ROOT_MAIL_DOMAIN} Subdomain=${aws_account_id} TemplateUrlPrefix=${TEMPLATE_URL_PREFIX} --capabilities CAPABILITY_AUTO_EXPAND CAPABILITY_IAM CAPABILITY_NAMED_IAM --no-fail-on-empty-changeset &
+aws --profile test_account_${aws_account_id} cloudformation deploy --stack-name superwerker --template-file components/superwerker.yaml --parameter-overrides Domain=${ROOT_MAIL_DOMAIN} Subdomain=${aws_account_id} TemplateUrlPrefix=${TEMPLATE_URL_PREFIX} --capabilities CAPABILITY_AUTO_EXPAND CAPABILITY_IAM --no-fail-on-empty-changeset &
 while ! domain_name_servers=$(aws --profile test_account_${aws_account_id} ssm get-parameter --name /superwerker/domain_name_servers --query Parameter.Value --output text); do sleep 10; done
 aws --profile ${SOURCE_PROFILE} cloudformation deploy --stack-name superwerker-pipeline-dns-wiring-${aws_account_id} --template-file tests/pipeline-dns-wiring.yaml --parameter-overrides RootMailDelegationTarget=$domain_name_servers RootMailDomain=${ROOT_MAIL_DOMAIN} RootMailSubdomain=${aws_account_id} --no-fail-on-empty-changeset
 sleep 3600 # give superwerker stack time to finish (Control Tower needs ~1h)
--- tests/update-test-env.sh
+++ tests/update-test-env.sh
@@ -8,4 +8,4 @@ template_url_prefix=https://superwerker-deployment.s3.amazonaws.com/${git_branch
 
 aws s3 sync ../components s3://superwerker-deployment/${git_branch}/components
 
-aws --profile test_account_${AWS_ACCOUNT_ID} cloudformation deploy --stack-name superwerker --template-file ../components/superwerker.yaml --parameter-overrides TemplateUrlPrefix=${template_url_prefix} --capabilities CAPABILITY_AUTO_EXPAND CAPABILITY_IAM CAPABILITY_NAMED_IAM --no-fail-on-empty-changeset
+aws --profile test_account_${AWS_ACCOUNT_ID} cloudformation deploy --stack-name superwerker --template-file ../components/superwerker.yaml --parameter-overrides TemplateUrlPrefix=${template_url_prefix} --capabilities CAPABILITY_AUTO_EXPAND CAPABILITY_IAM --no-fail-on-empty-changeset
