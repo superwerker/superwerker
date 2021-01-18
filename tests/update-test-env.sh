c50de6f4ecdcfdd3644849981a3818bee6f47e30
sbstjn <mail@sbstjn.com>
2021-01-18 12:14:45 +0100

    fix: add profile

--- tests/update-test-env.sh
+++ tests/update-test-env.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -euo pipefail
 
-echo AWS_ACCOUNT_ID: $AWS_ACCOUNT_ID
-
+superwerker_region=${SUPERWERKER_REGION:-"eu-central-1"}
 git_branch=$(git branch --show-current)
+
+echo AccountId ${AWS_ACCOUNT_ID} - Source Profile ${SOURCE_PROFILE} - Region ${superwerker_region} - Branch ${git_branch}
+
 template_url_prefix=https://superwerker-deployment.s3.amazonaws.com/${git_branch}
 
-aws s3 sync ../components s3://superwerker-deployment/${git_branch}/components
+aws --profile ${SOURCE_PROFILE} s3 sync ../components s3://superwerker-deployment/${git_branch}/components
 
-aws --profile test_account_${AWS_ACCOUNT_ID} cloudformation deploy --stack-name superwerker --template-file ../components/superwerker.yaml --parameter-overrides TemplateUrlPrefix=${template_url_prefix} --capabilities CAPABILITY_AUTO_EXPAND CAPABILITY_IAM --no-fail-on-empty-changeset
+aws --profile test_account_${AWS_ACCOUNT_ID} --region ${superwerker_region} cloudformation deploy --stack-name superwerker --template-file ../components/superwerker.yaml --parameter-overrides TemplateUrlPrefix=${template_url_prefix} --capabilities CAPABILITY_AUTO_EXPAND CAPABILITY_IAM --no-fail-on-empty-changeset
