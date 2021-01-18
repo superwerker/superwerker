8d2292928caa83105b2ec8f5255213d13f49d17d
Joern Barthel <joern.barthel@kreuzwerker.de>
2021-01-18 11:23:58 +0100

    Added federation link script.

---
+++ scripts/console.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+from urllib import parse, request
+import boto3
+import json
+
+creds = boto3.Session().get_credentials()
+url_credentials = dict(sessionId=creds.access_key,sessionKey=creds.secret_key, sessionToken=creds.token)
+
+request_parameters = "?Action=getSigninToken"
+request_parameters += "&DurationSeconds=43200"
+request_parameters += "&Session=" + parse.quote_plus(json.dumps(url_credentials))
+request_url = "https://signin.aws.amazon.com/federation" + request_parameters
+
+with request.urlopen(request_url) as response:
+    if not response.status == 200:
+        raise Exception("Failed to get federation token")
+    signin_token = json.loads(response.read())
+
+request_parameters = "?Action=login"
+request_parameters += "&Destination=" + parse.quote_plus("https://console.aws.amazon.com/")
+request_parameters += "&SigninToken=" + signin_token["SigninToken"]
+request_parameters += "&Issuer=" + parse.quote_plus("https://example.com")
+request_url = "https://signin.aws.amazon.com/federation" + request_parameters
+
+print(request_url)
