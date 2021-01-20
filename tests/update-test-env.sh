36c2807d9adf1916beb35b1f42b9259dfed27af8
Joern Barthel <joern.barthel@kreuzwerker.de>
2021-01-20 17:07:27 +0100

    Moved components to templates.

--- tests/update-test-env.sh
+++ tests/update-test-env.sh
@@ -8,6 +8,6 @@ echo AccountId ${AWS_ACCOUNT_ID} - Source Profile ${SOURCE_PROFILE} - Region ${s
 
 template_url_prefix=https://superwerker-deployment.s3.amazonaws.com/${git_branch}
 
-aws --profile ${SOURCE_PROFILE} s3 sync ../components s3://superwerker-deployment/${git_branch}/components
+aws --profile ${SOURCE_PROFILE} s3 sync ../templates s3://superwerker-deployment/${git_branch}/templates
 
-aws --profile test_account_${AWS_ACCOUNT_ID} --region ${superwerker_region} cloudformation deploy --stack-name superwerker --template-file ../components/superwerker.yaml --parameter-overrides TemplateUrlPrefix=${template_url_prefix} --capabilities CAPABILITY_AUTO_EXPAND CAPABILITY_IAM --no-fail-on-empty-changeset
+aws --profile test_account_${AWS_ACCOUNT_ID} --region ${superwerker_region} cloudformation deploy --stack-name superwerker --template-file ../templates/superwerker.yaml --parameter-overrides TemplateUrlPrefix=${template_url_prefix} --capabilities CAPABILITY_AUTO_EXPAND CAPABILITY_IAM --no-fail-on-empty-changeset
