f370e8355bdc7beabeea18f7598a834d8591dc1c
sbstjn <mail@sbstjn.com>
2021-02-04 06:52:21 +0100

    fix: update parameters for ci

--- tests/setup-test-env.sh
+++ tests/setup-test-env.sh
@@ -3,7 +3,9 @@ set -euo pipefail
 
 echo SOURCE_PROFILE: $SOURCE_PROFILE
 echo ROOT_MAIL_DOMAIN: $ROOT_MAIL_DOMAIN
-echo TEMPLATE_URL_PREFIX: $TEMPLATE_URL_PREFIX
+echo TEMPLATE_BUCKET_NAME: $TEMPLATE_BUCKET_NAME
+echo TEMPLATE_REGION: $TEMPLATE_REGION
+echo TEMPLATE_PREFIX: $TEMPLATE_PREFIX
 
 superwerker_region=${SUPERWERKER_REGION:-"eu-central-1"}
 
@@ -31,7 +33,7 @@ aws configure --profile test_account_${aws_account_id} --region ${superwerker_re
 aws sts get-caller-identity --profile test_account_${aws_account_id} --region ${superwerker_region} --no-cli-pager
 
 # setup superwerker in vended account
-aws --profile test_account_${aws_account_id} --region ${superwerker_region} cloudformation deploy --stack-name superwerker --template-file templates/superwerker.template.yaml --parameter-overrides Domain=${ROOT_MAIL_DOMAIN} Subdomain=${aws_account_id} TemplateUrlPrefix=${TEMPLATE_URL_PREFIX} --capabilities CAPABILITY_AUTO_EXPAND CAPABILITY_IAM --no-fail-on-empty-changeset &
+aws --profile test_account_${aws_account_id} --region ${superwerker_region} cloudformation deploy --stack-name superwerker --template-file templates/superwerker.template.yaml --parameter-overrides Domain=${ROOT_MAIL_DOMAIN} Subdomain=${aws_account_id} QSS3BucketName=${TEMPLATE_BUCKET_NAME} QSS3BucketRegion=${TEMPLATE_REGION} QSS3KeyPrefix=${TEMPLATE_PREFIX} --capabilities CAPABILITY_AUTO_EXPAND CAPABILITY_IAM --no-fail-on-empty-changeset &
 while ! domain_name_servers=$(aws --profile test_account_${aws_account_id} --region ${superwerker_region} ssm get-parameter --name /superwerker/domain_name_servers --query Parameter.Value --output text); do sleep 10; done
 aws --profile ${SOURCE_PROFILE} cloudformation deploy --stack-name superwerker-pipeline-dns-wiring-${aws_account_id} --template-file tests/pipeline-dns-wiring.yaml --parameter-overrides RootMailDelegationTarget=$domain_name_servers RootMailDomain=${ROOT_MAIL_DOMAIN} RootMailSubdomain=${aws_account_id} --no-fail-on-empty-changeset
 sleep 3600 # give superwerker stack time to finish (Control Tower needs ~1h)
