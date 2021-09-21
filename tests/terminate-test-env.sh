128ce09e34a3c79622821d49bbc40a5f7d673ff6
Reb-B <rb291.uni@gmail.com>
2021-09-21 10:11:53 +0200

    chore: empty buckets before trying to delete them

--- tests/terminate-test-env.sh
+++ tests/terminate-test-env.sh
@@ -26,6 +26,9 @@ npm i
 eval $(aws sts assume-role --profile $SOURCE_PROFILE --role-arn $aws_cross_account_role_arn --role-session-name test | jq -r '.Credentials | "export JS_AWS_ACCESS_KEY_ID=\(.AccessKeyId)\nexport JS_AWS_SECRET_ACCESS_KEY=\(.SecretAccessKey)\nexport JS_AWS_SESSION_TOKEN=\(.SessionToken)\n"')
 AWS_ACCESS_KEY_ID=$JS_AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$JS_AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN=$JS_AWS_SESSION_TOKEN AWS_REGION=${superwerker_region} CAPTCHA_KEY=$CAPTCHA_API_KEY node close-active-subaccounts.js
 
+# empty control tower and rootmail bucket
+for bucket in $(aws s3 ls --profile test_account_${AWS_ACCOUNT_ID} --region ${superwerker_region} | cut -d" " -f 3); do echo "Emptying bucket $bucket"; aws s3 rm --profile test_account_${AWS_ACCOUNT_ID} --region ${superwerker_region} s3://$bucket --recursive; done
+
 # remove stacks
 aws cloudformation delete-stack --profile $SOURCE_PROFILE --stack-name superwerker-pipeline-dns-wiring-${AWS_ACCOUNT_ID}  --no-cli-pager
 aws --profile test_account_${AWS_ACCOUNT_ID} --region ${superwerker_region} cloudformation delete-stack --stack-name superwerker  --no-cli-pager
