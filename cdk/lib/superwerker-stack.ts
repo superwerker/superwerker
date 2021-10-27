d1b249f005ced603343a33b5a1953b73e4a38841
Soenke Ruempler <soenke.ruempler@superluminar.io>
2021-10-27 20:27:45 +0200

    chore: introduce CDK for further feature development

---
+++ cdk/lib/superwerker-stack.ts
@@ -0,0 +1,8 @@
+import { Stack, StackProps} from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+
+export class SuperwerkerStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+  }
+}
