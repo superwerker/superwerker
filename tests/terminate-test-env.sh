085b1466aa1b1e1133d9ea1eb7dd9c36cb07f719
Reb-B <rb291.uni@gmail.com>
2021-11-03 14:26:56 +0100

    chore: identify script location to prevent breaking when trying to cd

--- tests/terminate-test-env.sh
+++ tests/terminate-test-env.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
 echo SOURCE_PROFILE: $SOURCE_PROFILE
 echo AWS_ACCOUNT_ID: $AWS_ACCOUNT_ID
 echo CAPTCHA_API_KEY: $CAPTCHA_API_KEY
-
 
 # check if source profile works
 aws sts get-caller-identity --profile $SOURCE_PROFILE --no-cli-pager
@@ -25,8 +26,9 @@ for superwerker_region_to_discover in eu-west-1 eu-central-1; do
 done
 
 # close sub-accounts so that the OVM can close the main / management account later
-cd ../tests/close-active-subaccounts
+cd $SCRIPT_DIR/close-active-subaccounts
 npm i
+
 # Node + ECS Container Creds + Assume Role doesn't seem to work, so work around by setting credentials
 eval $(aws sts assume-role --profile $SOURCE_PROFILE --role-arn $aws_cross_account_role_arn --role-session-name test | jq -r '.Credentials | "export JS_AWS_ACCESS_KEY_ID=\(.AccessKeyId)\nexport JS_AWS_SECRET_ACCESS_KEY=\(.SecretAccessKey)\nexport JS_AWS_SESSION_TOKEN=\(.SessionToken)\n"')
 AWS_ACCESS_KEY_ID=$JS_AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$JS_AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN=$JS_AWS_SESSION_TOKEN AWS_REGION=${superwerker_region} CAPTCHA_KEY=$CAPTCHA_API_KEY node close-active-subaccounts.js
