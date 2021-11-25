6189c068990c6ebcf40dd56e3b1932311514fd5b
Rebecca Burnside <35994499+Reb-B@users.noreply.github.com>
2021-11-25 11:39:50 +0100

    tests: script to remove unwanted stale test accounts (#268)

---
+++ tests/remove-stale-test-accounts.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -euo pipefail
+
+echo SOURCE_PROFILE: $SOURCE_PROFILE
+
+captcha_api_key=$(aws secretsmanager get-secret-value \
+--profile $SOURCE_PROFILE \
+--secret-id /superwerker/tests/2captcha_api_key \
+--region eu-west-1 \
+--query SecretString --output text)
+
+accounts=$(aws dynamodb query \
+--profile $SOURCE_PROFILE \
+--table-name account \
+--index-name account_status \
+--key-condition-expression "account_status = :name" \
+--expression-attribute-values '{":name":{"S":"VENDED"}}' \
+--query 'Items[].[account_id.S]' \
+--region eu-west-1 \
+--output text)
+
+for stale in ${accounts}
+do
+    echo $stale
+    SOURCE_PROFILE=superwerker-test1-master AWS_ACCOUNT_ID="${stale}" CAPTCHA_API_KEY=${captcha_api_key} ./terminate-test-env.sh
+done
--- tests/terminate-test-env.sh
+++ tests/terminate-test-env.sh
@@ -1,27 +1,34 @@
 #!/bin/bash
 set -euo pipefail
 
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
 echo SOURCE_PROFILE: $SOURCE_PROFILE
 echo AWS_ACCOUNT_ID: $AWS_ACCOUNT_ID
 echo CAPTCHA_API_KEY: $CAPTCHA_API_KEY
-
-superwerker_region=${SUPERWERKER_REGION:-"eu-central-1"}
 
 # check if source profile works
 aws sts get-caller-identity --profile $SOURCE_PROFILE --no-cli-pager
 
 aws_cross_account_role_arn="arn:aws:iam::${AWS_ACCOUNT_ID}:role/OVMCrossAccountRole"
 
-echo AccountId ${AWS_ACCOUNT_ID} - Cross Account Role ${aws_cross_account_role_arn} - Region ${superwerker_region}
+echo AccountId ${AWS_ACCOUNT_ID} - Cross Account Role ${aws_cross_account_role_arn}
 
 # setup AWS CLI to talk to vended account
-aws configure --profile test_account_${AWS_ACCOUNT_ID} --region ${superwerker_region} set role_arn $aws_cross_account_role_arn
-aws configure --profile test_account_${AWS_ACCOUNT_ID} --region ${superwerker_region} set source_profile ${SOURCE_PROFILE}
-aws sts get-caller-identity --profile test_account_${AWS_ACCOUNT_ID} --region ${superwerker_region} --no-cli-pager
+aws configure --profile test_account_${AWS_ACCOUNT_ID} set role_arn ${aws_cross_account_role_arn}
+aws configure --profile test_account_${AWS_ACCOUNT_ID} set source_profile ${SOURCE_PROFILE}
+aws sts get-caller-identity --profile test_account_${AWS_ACCOUNT_ID} --no-cli-pager
+for superwerker_region_to_discover in eu-west-1 eu-central-1; do
+    if aws --profile test_account_${AWS_ACCOUNT_ID} --region ${superwerker_region_to_discover} cloudformation describe-stacks --stack-name superwerker --no-cli-pager >/dev/null 2>&1; then
+        superwerker_region=${superwerker_region_to_discover}
+        break
+    fi
+done
 
 # close sub-accounts so that the OVM can close the main / management account later
-cd tests/close-active-subaccounts
+cd $SCRIPT_DIR/close-active-subaccounts
 npm i
+
 # Node + ECS Container Creds + Assume Role doesn't seem to work, so work around by setting credentials
 eval $(aws sts assume-role --profile $SOURCE_PROFILE --role-arn $aws_cross_account_role_arn --role-session-name test | jq -r '.Credentials | "export JS_AWS_ACCESS_KEY_ID=\(.AccessKeyId)\nexport JS_AWS_SECRET_ACCESS_KEY=\(.SecretAccessKey)\nexport JS_AWS_SESSION_TOKEN=\(.SessionToken)\n"')
 AWS_ACCESS_KEY_ID=$JS_AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$JS_AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN=$JS_AWS_SESSION_TOKEN AWS_REGION=${superwerker_region} CAPTCHA_KEY=$CAPTCHA_API_KEY node close-active-subaccounts.js
