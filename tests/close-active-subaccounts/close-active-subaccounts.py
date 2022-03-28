3edb1f77171945b90732b26b47d7db3f45668f6c
Soenke Ruempler <soenke.ruempler@superluminar.io>
2022-03-28 17:09:53 +0200

    tests: fix account closing
    by using fixed version of awsapilib

--- tests/close-active-subaccounts/close-active-subaccounts.py
+++ tests/close-active-subaccounts/close-active-subaccounts.py
@@ -9,21 +9,23 @@ PASSWORD = 'TesTIdontCare243!'
 
 org = boto3.client('organizations', region_name='us-east-1')
 ssm = boto3.client('ssm');
-solver = Captcha2(api_key=os.environ['CAPTCHA_KEY'])
+solver = Captcha2(os.environ['CAPTCHA_KEY'])
 password_manager = PasswordManager(solver=solver)
 
-@retry
-def close_account(account_to_close):
 
-    account_manager = AccountManager(account_to_close['Email'], PASSWORD, 'eu-west-1', solver=solver)
-
+@retry(stop_max_attempt_number=20)
+def reset_password(account_to_close):
     pw_reset_link_ssm_parameter_name = '/superwerker/rootmail/pw_reset_link/' + account_to_close['Email'].split('@')[0].split('+')[1]
     password_manager.request_password_reset(account_to_close['Email'])
     time.sleep(5)
     pw_reset_url = ssm.get_parameter(Name=pw_reset_link_ssm_parameter_name)['Parameter']['Value']
-
     password_manager.reset_password(pw_reset_url, PASSWORD)
 
+
+@retry(stop_max_attempt_number=20)
+def close_account(account_to_close):
+
+    account_manager = AccountManager(account_to_close['Email'], PASSWORD, 'us-east-1', solver=solver) # TODO: test us-east-1
     account_manager.terminate_account()
 
     print("Closed Account {}".format(account))
@@ -33,5 +35,6 @@ for account in accounts:
     if account['JoinedMethod'] != 'CREATED' or account['Status'] != 'ACTIVE':
         continue
 
+    reset_password(account)
     close_account(account)
 
