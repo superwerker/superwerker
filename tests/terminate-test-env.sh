399e50e05983b931ce23053cbe79b0e410b36344
Soenke Ruempler <soenke.ruempler@superluminar.io>
2022-04-29 11:13:15 +0200

    tests(terminate-test-env): try to repair rootmail if it has been already deleted

--- tests/terminate-test-env.sh
+++ tests/terminate-test-env.sh
@@ -25,6 +25,22 @@ for superwerker_region_to_discover in eu-west-1 eu-central-1; do
     fi
 done
 
+if [ -z "${superwerker_region}" ]; then
+    echo "No superwerker stack found in any region"
+    exit 1
+fi
+
+# check if rootmail needs repairing
+if ! stack_status=$(aws --profile test_account_${AWS_ACCOUNT_ID} --region ${superwerker_region} cloudformation describe-stacks --stack-name superwerker --no-cli-pager --output text --query 'Stacks[0].StackStatus' 2>/dev/null) || [[ $stack_status == DELETE* ]]; then
+  if ! aws --profile test_account_${AWS_ACCOUNT_ID} --region ${superwerker_region} cloudformation describe-stacks --stack-name temp-rootmail --no-cli-pager >/dev/null 2>&1; then
+      echo "superwerker stack not existing or deleted ($stack_status), trying to repair rootmail"
+      aws --profile test_account_${AWS_ACCOUNT_ID} --region ${superwerker_region} cloudformation create-stack --stack-name temp-rootmail --template-body file://templates/rootmail.yaml --parameters ParameterKey=Domain,ParameterValue=${ROOT_MAIL_DOMAIN} ParameterKey=Subdomain,ParameterValue=${AWS_ACCOUNT_ID} --capabilities CAPABILITY_AUTO_EXPAND CAPABILITY_IAM  --no-cli-pager
+      while ! domain_name_servers=$(aws --profile test_account_${AWS_ACCOUNT_ID} --region ${superwerker_region} ssm get-parameter --name /superwerker/domain_name_servers --query Parameter.Value --output text 2>/dev/null); do sleep 10; done
+      aws --profile ${SOURCE_PROFILE} cloudformation deploy --stack-name superwerker-pipeline-dns-wiring-temp-${AWS_ACCOUNT_ID} --template-file tests/pipeline-dns-wiring.yaml --parameter-overrides RootMailDelegationTarget=$domain_name_servers RootMailDomain=${ROOT_MAIL_DOMAIN} RootMailSubdomain=${AWS_ACCOUNT_ID} --no-fail-on-empty-changeset
+      aws --profile test_account_${AWS_ACCOUNT_ID} --region ${superwerker_region} cloudformation wait stack-create-complete --stack-name temp-rootmail
+  fi
+fi
+
 # close sub-accounts so that the OVM can close the main / management account later
 cd $SCRIPT_DIR/close-active-subaccounts
 virtualenv venv
@@ -36,3 +52,5 @@ AWS_PROFILE=test_account_${AWS_ACCOUNT_ID} AWS_DEFAULT_REGION=${superwerker_regi
 # remove stacks
 aws cloudformation delete-stack --profile $SOURCE_PROFILE --stack-name superwerker-pipeline-dns-wiring-${AWS_ACCOUNT_ID}  --no-cli-pager
 aws --profile test_account_${AWS_ACCOUNT_ID} --region ${superwerker_region} cloudformation delete-stack --stack-name superwerker  --no-cli-pager
+aws cloudformation delete-stack --profile $SOURCE_PROFILE --stack-name superwerker-pipeline-dns-wiring-temp-${AWS_ACCOUNT_ID}  --no-cli-pager
+aws --profile test_account_${AWS_ACCOUNT_ID} --region ${superwerker_region} cloudformation delete-stack --stack-name temp-rootmail  --no-cli-pager
