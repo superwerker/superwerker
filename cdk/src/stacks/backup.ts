d7deeb19e3f49d0fa177090a515ca55a43c8424e
sbstjn <mail@sbstjn.com>
2022-09-01 15:28:19 +0200

    move files

--- cdk/src/stacks/backup.ts
+++ cdk/src/stacks/backup.ts
@@ -3,11 +3,11 @@ import { NestedStack, NestedStackProps } from 'aws-cdk-lib';
 import { CfnInclude } from 'aws-cdk-lib/cloudformation-include';
 import { Construct } from 'constructs';
 
-export class ControlTowerStack extends NestedStack {
+export class BackupStack extends NestedStack {
   constructor(scope: Construct, id: string, props: NestedStackProps) {
     super(scope, id, props);
     new CfnInclude(this, 'SuperwerkerTemplate', {
-      templateFile: path.join(__dirname, '..', '..', 'templates', 'control-tower.yaml'),
+      templateFile: path.join(__dirname, '..', '..', '..', 'templates', 'backup.yaml'),
     });
   }
 }
