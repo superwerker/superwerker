64d18a0bc682d5f69ddc02653ba36228ab230032
sbstjn <mail@sbstjn.com>
2022-09-01 11:54:38 +0200

    wip

--- cdk/src/superwerker.ts
+++ cdk/src/superwerker.ts
@@ -1,6 +1,12 @@
-import { CfnCondition, CfnParameter, CfnStack, Fn, Stack, StackProps } from 'aws-cdk-lib';
+import { CfnCondition, CfnParameter, CfnStack, custom_resources, Fn, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { BudgetStack } from './budget';
+import { ControlTowerStack } from './control-tower';
+import { GuardDutyStack } from './guardduty';
+import { LivingDocumentationStack } from './living-documentation';
+import { NotificationsStack } from './notifications';
+import { RootmailStack } from './rootmail';
+import { SecurityHubStack } from './security-hub';
 
 export class SuperwerkerStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps) {
@@ -9,11 +15,10 @@ export class SuperwerkerStack extends Stack {
     Stack.of(this).templateOptions.description = 'Automated Best Practices for AWS Cloud setups - https://superwerker.cloud (qs-1rhrhoi4t)';
     Stack.of(this).templateOptions.metadata = {
       SuperwerkerVersion: '0.0.0-DEVELOPMENT',
-      QuickStartDocumentation:
-          {
-            EntrypointName: 'Parameters for launching Superwerker',
-            Order: '1',
-          },
+      QuickStartDocumentation: {
+        EntrypointName: 'Parameters for launching Superwerker',
+        Order: '1',
+      },
     };
 
     // @ts-ignore
@@ -77,6 +82,16 @@ export class SuperwerkerStack extends Stack {
       default: 'Yes',
     });
 
+    // Backup
+    const backupCondition = new CfnCondition(this, 'IncludeBackupCondition', {
+      expression: Fn.conditionEquals(includeBackup, 'Yes'),
+    });
+    backupCondition.overrideLogicalId('IncludeBackup');
+    const backupStack = new BudgetStack(this, 'Backup', {});
+    (backupStack.node.defaultChild as CfnStack).overrideLogicalId('Backup');
+    (backupStack.node.defaultChild as CfnStack).cfnOptions.condition = backupCondition;
+
+    // Budgets
     const budgetCondition = new CfnCondition(this, 'IncludeBudgetCondition', {
       expression: Fn.conditionEquals(includeBudget, 'Yes'),
     });
@@ -85,5 +100,100 @@ export class SuperwerkerStack extends Stack {
     (budgetStack.node.defaultChild as CfnStack).overrideLogicalId('Budget');
     (budgetStack.node.defaultChild as CfnStack).cfnOptions.condition = budgetCondition;
 
+    // GuardDuty
+    const guardDutyCondition = new CfnCondition(this, 'IncludeGuardDutyCondition', {
+      expression: Fn.conditionEquals(includeGuardDuty, 'Yes'),
+    });
+    guardDutyCondition.overrideLogicalId('IncludeGuardDuty');
+    const guardDutyStack = new GuardDutyStack(this, 'GuardDuty', {});
+    (guardDutyStack.node.defaultChild as CfnStack).overrideLogicalId('GuardDuty');
+    (guardDutyStack.node.defaultChild as CfnStack).cfnOptions.condition = guardDutyCondition;
+
+    // Notifications
+    const notificationsCondition = new CfnCondition(this, 'IncludeNotificationsCondition', {
+      expression: Fn.conditionNot(Fn.conditionEquals(notificationsMail, '')),
+    });
+    notificationsCondition.overrideLogicalId('IncludeNotifications');
+    const notificationsStack = new NotificationsStack(this, 'Notifications', {});
+    (notificationsStack.node.defaultChild as CfnStack).overrideLogicalId('Notifications');
+    (notificationsStack.node.defaultChild as CfnStack).cfnOptions.condition = notificationsCondition;
+
+    // SecurityHub
+    const securityHubCondition = new CfnCondition(this, 'IncludeSecurityHubCondition', {
+      expression: Fn.conditionEquals(includeSecurityHub, 'Yes'),
+    });
+    securityHubCondition.overrideLogicalId('IncludeSecurityHub');
+    const securityHubStack = new SecurityHubStack(this, 'SecurityHub', {});
+    (securityHubStack.node.defaultChild as CfnStack).overrideLogicalId('SecurityHub');
+    (securityHubStack.node.defaultChild as CfnStack).cfnOptions.condition = securityHubCondition;
+
+    // ServiceControlPolicies
+    const serviceControlPoliciesCondition = new CfnCondition(this, 'IncludeServiceControlPoliciesCondition', {
+      expression: Fn.conditionEquals(includeServiceControlPolicies, 'Yes'),
+    });
+    serviceControlPoliciesCondition.overrideLogicalId('IncludeServiceControlPolicies');
+    const serviceControlPoliciesStack = new SecurityHubStack(this, 'ServiceControlPolicies', {});
+    (serviceControlPoliciesStack.node.defaultChild as CfnStack).overrideLogicalId('ServiceControlPolicies');
+    (serviceControlPoliciesStack.node.defaultChild as CfnStack).cfnOptions.condition = serviceControlPoliciesCondition;
+
+    /**
+     * Core Components
+     */
+
+    // RootMail
+    const rootMailStack = new RootmailStack(this, 'RootMail', {});
+    (rootMailStack.node.defaultChild as CfnStack).overrideLogicalId('RootMail');
+
+    const emailAudit = new custom_resources.AwsCustomResource(this, 'GeneratedAuditAWSAccountEmail', {
+      onUpdate: {
+        service: 'lambda',
+        action: 'invoke',
+        parameters: {
+          FunctionName: (rootMailStack.node.defaultChild as CfnStack).getAtt('Outputs.EmailGeneratorFunction'),
+          Payload: {
+            Purpose: 'audit',
+          },
+        },
+        physicalResourceId: custom_resources.PhysicalResourceId.of(Date.now().toString()), // Update physical id to always fetch the latest version
+      },
+      policy: custom_resources.AwsCustomResourcePolicy.fromSdkCalls({
+        resources: custom_resources.AwsCustomResourcePolicy.ANY_RESOURCE,
+      }),
+    });
+
+    // (emailAudit.node.defaultChild as CfnResource).overrideLogicalId('GeneratedAuditAWSAccountEmail');
+
+    const emailLogArchive = new custom_resources.AwsCustomResource(this, 'GeneratedLogArchiveAWSAccountEmail', {
+      onUpdate: {
+        // will also be called for a CREATE event
+        service: 'lambda',
+        action: 'invoke',
+        parameters: {
+          FunctionName: (rootMailStack.node.defaultChild as CfnStack).getAtt('Outputs.EmailGeneratorFunction'),
+          Payload: {
+            Purpose: 'logarchive',
+          },
+        },
+        physicalResourceId: custom_resources.PhysicalResourceId.of(Date.now().toString()), // Update physical id to always fetch the latest version
+      },
+      policy: custom_resources.AwsCustomResourcePolicy.fromSdkCalls({
+        resources: custom_resources.AwsCustomResourcePolicy.ANY_RESOURCE,
+      }),
+    });
+
+    // (emailLogArchive.node.defaultChild as CfnResource).overrideLogicalId('GeneratedLogArchiveAWSAccountEmail');
+
+    // ControlTower
+    const controlTowerStack = new ControlTowerStack(this, 'ControlTower', {
+      parameters: {
+        AuditAWSAccountEmail: emailAudit.getResponseField('email'),
+        LogArchiveAWSAccountEmail: emailLogArchive.getResponseField('email'),
+      },
+    });
+    (controlTowerStack.node.defaultChild as CfnStack).overrideLogicalId('ControlTower');
+
+    // LivingDocumentation
+    const livingDocumentationStack = new LivingDocumentationStack(this, 'LivingDocumentation', {});
+    (livingDocumentationStack.node.defaultChild as CfnStack).overrideLogicalId('LivingDocumentation');
   }
 }
--- cdk/test/superwerker.test.ts
+++ cdk/test/superwerker.test.ts
@@ -18,7 +18,7 @@ describe('parameters', () => {
   const app = new App();
   const originalStack = new OriginalStack(app, 'original', {});
   const stack = new SuperwerkerStack(app, 'stack', {});
-  const expectedParameters = Template.fromStack(originalStack).toJSON().Parameters as {[key:string]: {[key:string]: string}};
+  const expectedParameters = Template.fromStack(originalStack).toJSON().Parameters as { [key: string]: { [key: string]: string } };
   // Ignore the quickstart stuff for now
   for (const key in expectedParameters) {
     if (key.startsWith('QS')) delete expectedParameters[key];
@@ -33,13 +33,18 @@ describe('resources', () => {
   const originalStack = new OriginalStack(app, 'original', {});
   const stack = new SuperwerkerStack(app, 'stack', {});
   console.log(Template.fromStack(stack).toJSON());
-  const expectedResources = Template.fromStack(originalStack).toJSON().Resources as {[key:string]: {[key:string]: string}};
-  test.each(Object.entries(expectedResources))('resource: %p', (param, value) => {
+  const expectedResources = Template.fromStack(originalStack).toJSON().Resources as { [key: string]: { [key: string]: string } };
+  test.each(Object.entries(expectedResources))('resource: %p', (resource, resourceProps) => {
     // This sucks. Unfortunately we can't just call 'hasResource('myLogicalId').
     // TODO: make this better, either extend Template to have a better matcher or come up with a helper method.
     // Maybe like this: https://www.emgoto.com/jest-partial-match/
     // https://cdk-dev.slack.com/archives/C018XT6REKT/p1662017721195839
     // For now we just check that the logical id and the condition are the same
-    expect(Template.fromStack(stack).toJSON().Resources).toHaveProperty([param, 'Condition'], value.Condition);
+
+    if (resourceProps.Condition) {
+      expect(Template.fromStack(stack).toJSON().Resources).toHaveProperty([resource, 'Condition'], resourceProps.Condition);
+    } else {
+      expect(Template.fromStack(stack).toJSON().Resources).toHaveProperty(resource);
+    }
   });
 });
