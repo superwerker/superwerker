05ace7fb952089880b7ea5dd5ae20a84b9bc930d
Jan Brauer <jan.brauer@superluminar.io>
2022-09-01 09:00:30 +0200

    Port parameters of SuperwerkerStack

--- cdk/src/main.ts
+++ cdk/src/main.ts
@@ -1,16 +1,6 @@
 import { App, CliCredentialsStackSynthesizer } from 'aws-cdk-lib';
 import { SuperwerkerStack } from './superwerker';
 
-// export class SuperwerkerStack extends Stack {
-//   constructor(scope: Construct, id: string, props: StackProps) {
-//     super(scope, id, props);
-//
-//     new CfnInclude(this, 'SuperwerkerTemplate', {
-//       templateFile: path.join(__dirname, '..', '..', 'templates', 'superwerker.template.yaml'),
-//     });
-//   }
-// }
-
 // for development, use account/region from cdk cli
 const devEnv = {
   account: process.env.CDK_DEFAULT_ACCOUNT,
