10fee952110ecb9d940003732b6be5f0319c0769
sbstjn <mail@sbstjn.com>
2022-09-02 10:36:42 +0200

    wip

--- cdk/src/index.ts
+++ cdk/src/index.ts
@@ -2,18 +2,29 @@ import { App, CliCredentialsStackSynthesizer } from 'aws-cdk-lib';
 import { SuperwerkerStack } from './stacks/superwerker';
 
 // for development, use account/region from cdk cli
-const devEnv = {
-  account: process.env.CDK_DEFAULT_ACCOUNT,
-  region: process.env.CDK_DEFAULT_REGION,
-};
+// const devEnv = {
+//   account: process.env.CDK_DEFAULT_ACCOUNT,
+//   // region: process.env.CDK_DEFAULT_REGION,
+// };
+
+// const region1Env = {
+//   account: process.env.CDK_DEFAULT_ACCOUNT,
+//   region: 'us-east-1',
+// };
+
+// const region2Env = {
+//   account: process.env.CDK_DEFAULT_ACCOUNT,
+//   region: 'af-south-1',
+// };
+
+// const regions = [region1Env, region2Env];
 
 const app = new App();
 
 new SuperwerkerStack(app, 'SuperwerkerStack', {
-  env: devEnv,
+  // env: devEnv,
   synthesizer: new CliCredentialsStackSynthesizer({
-    fileAssetsBucketName: 'superwerker-assets12123-bucket',
-    bucketPrefix: 'example',
+    fileAssetsBucketName: 'superwerker-assets-${AWS::Region}',
   }),
 });
 
