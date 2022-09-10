8b6510cef58f36b219eafdce265cc49d42b17013
Jan Brauer <jan.brauer@superluminar.io>
2022-09-10 14:56:27 +0200

    Allow to pass SUPERWERKER_VERSION as env var

--- cdk/src/index.ts
+++ cdk/src/index.ts
@@ -3,9 +3,13 @@ import { SuperwerkerStack } from './stacks/superwerker';
 
 const app = new App();
 
+const superwerkerVersion = process.env.SUPERWERKER_VERSION || '0.0.0-DEVELOPMENT';
+
 new SuperwerkerStack(app, 'SuperwerkerStack', {
+  version: superwerkerVersion,
   synthesizer: new CliCredentialsStackSynthesizer({
     fileAssetsBucketName: 'superwerker-assets-${AWS::Region}',
+    bucketPrefix: `${superwerkerVersion}/`,
   }),
 });
 
