20918c9aa88a94510c3f1859f1abc19e5502b170
Jan Brauer <jan.brauer@superluminar.io>
2022-10-04 08:35:30 +0200

    Hardcode region list
    List has beeen created with scripts/ct+sso.sh

--- cdk/src/scripts/publish-assets.ts
+++ cdk/src/scripts/publish-assets.ts
@@ -1,18 +1,29 @@
 import { exec } from 'child_process';
 import * as path from 'path';
-import AWS from 'aws-sdk';
+
+const REGIONS = [
+  "ap-northeast-1",
+  "ap-northeast-2",
+  "ap-south-1",
+  "ap-southeast-1",
+  "ap-southeast-2",
+  "ca-central-1",
+  "eu-central-1",
+  "eu-north-1",
+  "eu-west-1",
+  "eu-west-2",
+  "eu-west-3",
+  "sa-east-1",
+  "us-east-1",
+  "us-east-2",
+  "us-west-2"
+]
 
 // Publish assets into all regional buckets
 // e.g. superwerker-assets-eu-central-1 etc.
 const main = async () => {
   const assetManifestPath = path.resolve(__dirname, '..', '..', 'cdk.out', 'SuperwerkerStack.assets.json');
-
-  // Fetch all enabled regions from EC2
-  // In our master account we have enabled all regions
-  const ec2Client = new AWS.EC2({ region: 'eu-central-1' });
-  const regions = (await ec2Client.describeRegions().promise()).Regions!.map((r) => r.RegionName);
-
-  for (const region of regions) {
+  for (const region of REGIONS) {
     const command = `AWS_REGION=${region} yarn cdk-assets publish -p ${assetManifestPath}`;
     console.log(command);
     await exec(command, (err, stdout, stderr) => {
@@ -26,7 +37,7 @@ const main = async () => {
 };
 
 // top level await madness
-(async () => {await main();})().catch(e => {
+(async () => { await main(); })().catch(e => {
   console.log(e);
 });
 
