a247d805cc1d66b6d1cb5b0b69abae390706761f
Joscha <joscha.meyer@superluminar.io>
2023-01-24 15:51:57 +0100

    use close-account API for sub-account closing

--- tests/close-active-subaccounts/close-active-subaccounts.py
+++ tests/close-active-subaccounts/close-active-subaccounts.py
@@ -1,35 +1,20 @@
-from awsapilib import AccountManager, PasswordManager
-from awsapilib.captcha import Captcha2
 import boto3
 import botocore
-import os
-import time
-from retrying import retry
-
-PASSWORD = 'TesTIdontCare243!'
-
 org = boto3.client('organizations', region_name='us-east-1')
-ssm = boto3.client('ssm');
-solver = Captcha2(os.environ['CAPTCHA_KEY'])
-password_manager = PasswordManager(solver=solver)
 
 
-@retry(stop_max_attempt_number=100)
-def reset_password(account_to_close):
-    pw_reset_link_ssm_parameter_name = '/superwerker/rootmail/pw_reset_link/' + account_to_close['Email'].split('@')[0].split('+')[1]
-    password_manager.request_password_reset(account_to_close['Email'])
-    time.sleep(5)
-    pw_reset_url = ssm.get_parameter(Name=pw_reset_link_ssm_parameter_name)['Parameter']['Value']
-    password_manager.reset_password(pw_reset_url, PASSWORD)
+def organizations_close_account(account):
+    print(account)
+    try:
+        response = org.close_account(
+            AccountId=account['Id']
+        )
+        print(response)
+    except org.exceptions.AccountAlreadyClosedException:
+        print("Account already closed")
+    except botocore.exceptions.ClientError as e:
+        print(e)
 
-
-@retry(stop_max_attempt_number=100)
-def close_account(account_to_close):
-
-    account_manager = AccountManager(account_to_close['Email'], PASSWORD, 'us-east-1', solver=solver)
-    account_manager.terminate_account()
-
-    print("Closed Account {}".format(account))
 
 try:
     accounts = org.list_accounts()['Accounts']
@@ -43,6 +28,4 @@ for account in accounts:
     if account['JoinedMethod'] != 'CREATED' or account['Status'] != 'ACTIVE':
         continue
 
-    reset_password(account)
-    close_account(account)
-
+    organizations_close_account(account)
