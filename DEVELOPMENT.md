e28724a7b6d153b4f6e72b27b7b09dc9ace967c1
Manuel Vogel <mavogel@posteo.de>
2023-02-13 14:02:09 +0100

    feat: add udpate env for cdk and notes on dev flow for externals
    Signed-off-by: Manuel Vogel <mavogel@posteo.de>

--- DEVELOPMENT.md
+++ DEVELOPMENT.md
@@ -99,7 +99,7 @@ ORGANIZATIONS_VENDING_MACHINE_ENDPOINT=... \
 
 #### Update the test environment
 
-This becomes handy if you directly want to deploy your changes to the test environment:
+This becomes handy if you directly want to deploy your changes to the **old** test environment with the `yaml` files:
 
 ```bash
 SOURCE_PROFILE=... \
@@ -108,12 +108,36 @@ SOURCE_PROFILE=... \
   ./tests/update-test-env.sh
 ```
 
+For the `cdk` stack
+```bash
+# we assume you have your environment variables set
+# AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_SESSION_TOKEN
+SOURCE_PROFILE=... \
+  SUPERWERKER_REGION=uk-east-1 \
+  AWS_ACCOUNT_ID=... \
+  ./tests/update-test-env-cdk.sh
+```
+
+and for `kreuzwerker` or any other party assuming a role in the `SuperwerkerTestMaster` account:
+
+```bash
+SOURCE_PROFILE=YourSandboxAdmin \
+  ROLE_TO_ASSUME=<xyz> \
+  SUPERWERKER_REGION=uk-east-1 \
+  AWS_ACCOUNT_ID=... \
+  ./tests/update-test-env-cdk.sh
+```
+
 #### Run tests
 
 This runs the python integration tests. Also run `yarn test` before for the unit tests
 
 ```bash
 cd tests
+
+virtualenv venv
+./venv/bin/activate
+
 ACCOUNT_FACTORY_ACCOUNT_ID=... \
   AWS_DEFAULT_REGION=uk-east-1 \
   AWS_PROFILE=test_account_... \
@@ -129,7 +153,7 @@ This uses `firefox` and logs you in to the test account. You get the `AWS_ACCOUN
  
 ```bash
 SUPERWERKER_REGION=uk-east-1 \
-  SOURCE_PROFILE=... \
+  SOURCE_PROFILE=SuperwerkerTestMaster \
   AWS_ACCOUNT_ID=... \
   ./tests/login-test-env.sh
 ```
---
+++ tests/update-test-env-cdk.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+set -euo pipefail
+
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+superwerker_region=${SUPERWERKER_REGION:-"eu-central-1"}
+git_long_commit_hash=$(git rev-parse HEAD)
+
+
+TEMPLATE_PREFIX=build/${git_long_commit_hash}
+# used in 'yarn synth'
+export SUPERWERKER_VERSION=${git_long_commit_hash}
+
+cd ${SCRIPT_DIR}/../cdk
+yarn synth
+
+# if you have to assume a role in the 'SuperwerkerTestMaster' AWS account
+if [ -n "${ROLE_TO_ASSUME}" ]; then
+    echo AccountId ${AWS_ACCOUNT_ID} - Source Profile ${SOURCE_PROFILE} with role to assume ${ROLE_TO_ASSUME} - Region ${superwerker_region} - Git Hash ${git_long_commit_hash}
+    
+    TMP_AWS_SW_CREDENTIALS=$(aws sts assume-role --profile ${SOURCE_PROFILE} --role-arn "arn:aws:iam::824014778649:role/${ROLE_TO_ASSUME}" --role-session-name test-stack-update)
+    export AWS_ACCESS_KEY_ID=$(echo "$TMP_AWS_SW_CREDENTIALS" | jq -r '.Credentials.AccessKeyId' | tr -d '\r\n')
+    export AWS_SECRET_ACCESS_KEY=$(echo "$TMP_AWS_SW_CREDENTIALS" | jq -r '.Credentials.SecretAccessKey' | tr -d '\r\n')
+    export AWS_SESSION_TOKEN=$(echo "$TMP_AWS_SW_CREDENTIALS" | jq -r '.Credentials.SessionToken' | tr -d '\r\n')
+else 
+    echo AccountId ${AWS_ACCOUNT_ID} - Source Profile ${SOURCE_PROFILE} - Region ${superwerker_region} - Git Hash ${git_long_commit_hash}
+fi
+
+# as the script needs to have the AWS_ACCESS_KEY_ID etc. to be set
+yarn publish-assets
+
+# from buildspec.yaml an we use the same credentials like for 'yarn publish-assets'
+aws s3 cp ${SCRIPT_DIR}/../cdk/cdk.out/SuperwerkerStack.template.json s3://superwerker-deployment/${TEMPLATE_PREFIX}/templates/superwerker.template.yaml
+aws s3 cp ${SCRIPT_DIR}/../cdk/cdk.out/SuperwerkerStack.template.json s3://superwerker-deployment/${TEMPLATE_PREFIX}/templates/
+
+# clean the temporary credentials
+unset AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN
+
+echo "----------------------------------------------------------------"
+echo "NOTE: for now update the stack by hand in firefox with new template url 'https://superwerker-deployment.s3.${superwerker_region}.amazonaws.com/${TEMPLATE_PREFIX}/templates/SuperwerkerStack.template.json'"
+# we update the stack in the AWS account under test
+#aws --profile test_account_${AWS_ACCOUNT_ID} --region ${superwerker_region} cloudformation update-stack --stack-name superwerker --template-url "https://superwerker-deployment.s3.${superwerker_region}.amazonaws.com/${TEMPLATE_PREFIX}/templates/SuperwerkerStack.template.json"  --capabilities CAPABILITY_AUTO_EXPAND CAPABILITY_IAM --no-cli-pager                                                                                                                                          
