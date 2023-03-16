6675233606f370bf5976fbf6b77d1f087814c03b
Aleksa Perovic <aleksa.perovic1@gmail.com>
2023-03-16 16:48:35 +0100

    fix: escape cw dashboard string

--- cdk/src/functions/living-docs-dashboard-generator.ts
+++ cdk/src/functions/living-docs-dashboard-generator.ts
@@ -41,14 +41,28 @@ export async function handler(_event: any, _context: any) {
       dnsDelegationText = '### DNS Setup pending';
     }
   }
-  const finalDashboardMessage = generateFinalDashboardMessage(dnsDelegationText, dnsDomain, awsRegion);
+  const finalDashboardMessage = generateFinalDashboardMessage(dnsDelegationText, dnsDomain!, awsRegion!);
+  const finalDashboardMessageEscaped = escape_string(finalDashboardMessage)
 
   await cloudwatch.putDashboard({
     DashboardName: 'superwerker',
-    DashboardBody: JSON.parse(`{"widgets": [{"type": "text","x": 0,"y": 0,"width": 24,"height": 20,"properties": {"markdown": ${finalDashboardMessage}}}]}`),
+    DashboardBody: `{"widgets": [{"type": "text","x": 0,"y": 0,"width": 24,"height": 20,"properties": {"markdown": "${finalDashboardMessageEscaped}"}}]}`,
   }).promise();
 
 }
+
+function escape_string (input: string) {
+  return input
+    .replace(/[\\]/g, '\\\\')
+    .replace(/[\"]/g, '\\\"')
+    .replace(/[\/]/g, '\\/')
+    .replace(/[\b]/g, '\\b')
+    .replace(/[\f]/g, '\\f')
+    .replace(/[\n]/g, '\\n')
+    .replace(/[\r]/g, '\\r')
+    .replace(/[\t]/g, '\\t')
+    .replace(/[\u0000-\u0019]+/g,"");
+};
 
 function generateSuccesfulDnsConfigurationMessage(dnsDomain: string) {
   return endent`
@@ -79,48 +93,48 @@ function generateDnsConfigurationRequiredMessage(dnsDomain: string, ns: string[]
 function generateFinalDashboardMessage(dnsDelegationText: string, dnsDomain: string, region: string) {
   const currentTime=new Date();
   return endent`
-    # [superwerker](https://github.com/superwerker/superwerker)
-    &nbsp;
+  # [superwerker](https://github.com/superwerker/superwerker)
+  &nbsp;
 
-    ${dnsDelegationText}
+  ${dnsDelegationText}
 
-    &nbsp;
-    ## Next steps - finish setup
-    &nbsp;
+  &nbsp;
+  ## Next steps - finish setup
+  &nbsp;
 
-    ### SSO Setup
+  ### SSO Setup
 
-    - Check your e-mail inbox for "Invitation to join AWS Single Sign-On" and follow the setups to accept the invitation. After finishing, log in into AWS via the AWS SSO portal.
-    - [Configure AWS SSO with identity providers](https://docs.aws.amazon.com/singlesignon/latest/userguide/manage-your-identity-source-idp.html), e.g. [Azure AD](https://controltower.aws-management.tools/aa/sso/azure_ad/), [Google Workspace](https://controltower.aws-management.tools/aa/sso/google/), [Okta](https://controltower.aws-management.tools/aa/sso/okta/), [OneLogin](https://controltower.aws-management.tools/aa/sso/onelogin/), to login to AWS with your existing login mechanisms.
+  - Check your e-mail inbox for \'Invitation to join AWS Single Sign-On\' and follow the setups to accept the invitation. After finishing, log in into AWS via the AWS SSO portal.
+  - [Configure AWS SSO with identity providers](https://docs.aws.amazon.com/singlesignon/latest/userguide/manage-your-identity-source-idp.html), e.g. [Azure AD](https://controltower.aws-management.tools/aa/sso/azure_ad/), [Google Workspace](https://controltower.aws-management.tools/aa/sso/google/), [Okta](https://controltower.aws-management.tools/aa/sso/okta/), [OneLogin](https://controltower.aws-management.tools/aa/sso/onelogin/), to login to AWS with your existing login mechanisms.
 
-    &nbsp;
-    ### Organizations Setup
+  &nbsp;
+  ### Organizations Setup
 
-    - Set up recommended organizational units via [Control Tower](/controltower/home/organizationunits?region=${region}) acording to the [Organizing Your AWS Environment Using Multiple Accounts whitepaper](https://docs.aws.amazon.com/whitepapers/latest/organizing-your-aws-environment/production-starter-organization.html)
-        - Create a \`Workloads_Prod\` organizational unit for production workloads
-        - Create a \`Workloads_Test\` organizational unit for test/dev workloads
+  - Set up recommended organizational units via [Control Tower](/controltower/home/organizationunits?region=${region}) acording to the [Organizing Your AWS Environment Using Multiple Accounts whitepaper](https://docs.aws.amazon.com/whitepapers/latest/organizing-your-aws-environment/production-starter-organization.html)
+      - Create a \`Workloads_Prod\` organizational unit for production workloads
+      - Create a \`Workloads_Test\` organizational unit for test/dev workloads
 
-    &nbsp;
-    ## What now? Standard operating procedures
+  &nbsp;
+  ## What now? Standard operating procedures
 
-    - Create AWS accounts for each of your workloads via the [Control Tower Account Factory](/controltower/home/accountfactory/createAccount?region=${region}) (for "Account email" use \`root+<random_suffix>@${dnsDomain}\`)
-    - Check [OpsCenter for incoming events and messages](/systems-manager/opsitems?region=${region}#list_ops_items_filters=Status:Equal:Open_InProgress&activeTab=OPS_ITEMS)
-    - Check [AWS Security Hub](/securityhub/home?region=${region}) for security best practise violations (login to Audit Account via AWS SSO portal first)
-    - Check [Amazon GuardDuty](/guardduty/home?region=${region}#/findings) for threats against your AWS accounts (login to Audit Account via AWS SSO portal first)
-    - Exclude resources from being backed-up by changing the \`superwerker:backup\` tag to \`none\`
+  - Create AWS accounts for each of your workloads via the [Control Tower Account Factory](/controltower/home/accountfactory/createAccount?region=${region}) (for \'Account email\' use \`root+<random_suffix>@${dnsDomain}\`)
+  - Check [OpsCenter for incoming events and messages](/systems-manager/opsitems?region=${region}#list_ops_items_filters=Status:Equal:Open_InProgress&activeTab=OPS_ITEMS)
+  - Check [AWS Security Hub](/securityhub/home?region=${region}) for security best practise violations (login to Audit Account via AWS SSO portal first)
+  - Check [Amazon GuardDuty](/guardduty/home?region=${region}#/findings) for threats against your AWS accounts (login to Audit Account via AWS SSO portal first)
+  - Exclude resources from being backed-up by changing the \`superwerker:backup\` tag to \`none\`
 
-    &nbsp;
-    ## Help and more information
+  &nbsp;
+  ## Help and more information
 
-    - [superwerker on GitHub](https://github.com/superwerker/superwerker)
-    - [Architecture Decision Records](https://github.com/superwerker/superwerker/tree/main/docs/adrs)
-    - [#superwerker](https://og-aws.slack.com/archives/C01CQ34TC93) Slack channel in [og-aws](http://slackhatesthe.cloud)
-    - [Mailing list](https://groups.google.com/forum/#!forum/superwerker/join)
+  - [superwerker on GitHub](https://github.com/superwerker/superwerker)
+  - [Architecture Decision Records](https://github.com/superwerker/superwerker/tree/main/docs/adrs)
+  - [#superwerker](https://og-aws.slack.com/archives/C01CQ34TC93) Slack channel in [og-aws](http://slackhatesthe.cloud)
+  - [Mailing list](https://groups.google.com/forum/#!forum/superwerker/join)
 
-    &nbsp;
+  &nbsp;
 
-    \`\`\`
-    Updated at ${currentTime} (use browser reload to refresh)
-    \`\`\`
-    `;
+  \`\`\`
+  Updated at ${currentTime} (use browser reload to refresh)
+  \`\`\`
+  `;
 }
