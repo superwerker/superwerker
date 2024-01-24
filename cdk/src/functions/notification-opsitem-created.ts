d72b120e96ea2d3d2354a03934cc95323898dc31
gauravdubey <gaurav.dubey@kreuzwerker.de>
2024-01-24 12:43:34 +0100

    build: renamed variables in notification-opsitem-created.ts to camelcase

--- cdk/src/functions/notification-opsitem-created.ts
+++ cdk/src/functions/notification-opsitem-created.ts
@@ -2,31 +2,31 @@ import { SNSClient, PublishCommand } from '@aws-sdk/client-sns';
 
 const snsClient = new SNSClient();
 
-export async function handler(_event:any, _context:any) {
+export async function handler(event:any, _context:any) {
   const region = process.env.AWS_REGION;
-  const response_elements = _event.detail.responseElements;
-  const id = response_elements.OpsItemId;
-  const request_parameters = _event.detail.requestParameters;
-  const desc = request_parameters.description;
-  const title = request_parameters.title;
+  const responseElements = event.detail.responseElements;
+  const id = responseElements.OpsItemId;
+  const requestParameters = event.detail.requestParameters;
+  const desc = requestParameters.description;
+  const title = requestParameters.title;
   const url = `https://${region}.console.aws.amazon.com/systems-manager/opsitems/${id}`;
   const topicARN = process.env.TOPIC_ARN;
 
   log({
     desc: desc,
-    event: _event,
+    event: event,
     level: 'info',
     msg: 'Publishing new ops item event from CloudTrail to SNS',
     title: title,
     url: url,
   });
 
-  const message_title = `New OpsItem: ${title}`;
-  const message_body = `${desc}\n\n${url}`;
+  const messageTitle = `New OpsItem: ${title}`;
+  const messageBody = `${desc}\n\n${url}`;
 
   await snsClient.send(new PublishCommand({
-    Message: message_body,
-    Subject: message_title,
+    Message: messageBody,
+    Subject: messageTitle,
     TopicArn: topicARN, //process.env.TOPIC_ARN,
   }));
 }
