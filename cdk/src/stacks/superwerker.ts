c00f0911a17d36f5a1b38821f5eeb7828bc02b11
Aleksa Perovic <aleksa.perovic1@gmail.com>
2024-02-14 01:20:50 +0100

    chore: comment out all resources except prepare and controltower for testing

--- cdk/src/stacks/superwerker.ts
+++ cdk/src/stacks/superwerker.ts
@@ -1,15 +1,15 @@
-import { CfnCondition, CfnParameter, CfnStack, Fn, Stack, StackProps } from 'aws-cdk-lib';
+import { CfnStack, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { BackupStack } from './backup';
-import { BudgetStack } from './budget';
+// import { BackupStack } from './backup';
+// import { BudgetStack } from './budget';
 import { ControlTowerStack } from './control-tower';
-import { GuardDutyStack } from './guardduty';
-import { LivingDocumentationStack } from './living-documentation';
-import { NotificationsStack } from './notifications';
-import { RootmailStack } from './rootmail';
-import { SecurityHubStack } from './security-hub';
-import { ServiceControlPoliciesStack } from './sevice-control-policies';
-import { GenerateEmailAddress } from '../constructs/generate-email-address';
+import { PrepareStack } from './prepare';
+// import { GuardDutyStack } from './guardduty';
+// import { LivingDocumentationStack } from './living-documentation';
+// import { NotificationsStack } from './notifications';
+// import { RootmailStack } from './rootmail';
+// import { SecurityHubStack } from './security-hub';
+// import { ServiceControlPoliciesStack } from './sevice-control-policies';
 
 export interface SuperwerkerStackProps extends StackProps {
   readonly version?: string;
@@ -31,177 +31,177 @@ export class SuperwerkerStack extends Stack {
       },
     };
 
-    const domain = new CfnParameter(this, 'Domain', {
-      type: 'String',
-      description:
-        'Domain used for root mail feature. Please see https://github.com/superwerker/superwerker/blob/main/README.md#technical-faq for more information',
-    });
+    // const domain = new CfnParameter(this, 'Domain', {
+    //   type: 'String',
+    //   description:
+    //     'Domain used for root mail feature. Please see https://github.com/superwerker/superwerker/blob/main/README.md#technical-faq for more information',
+    // });
 
-    const subdomain = new CfnParameter(this, 'Subdomain', {
-      type: 'String',
-      description:
-        'Subdomain used for root mail feature. Please see https://github.com/superwerker/superwerker/blob/main/README.md#technical-faq for more information',
-      default: 'aws',
-    });
+    // const subdomain = new CfnParameter(this, 'Subdomain', {
+    //   type: 'String',
+    //   description:
+    //     'Subdomain used for root mail feature. Please see https://github.com/superwerker/superwerker/blob/main/README.md#technical-faq for more information',
+    //   default: 'aws',
+    // });
 
-    const notificationsMail = new CfnParameter(this, 'NotificationsMail', {
-      type: 'String',
-      description:
-        'Mail address used for notifications. Please see https://github.com/superwerker/superwerker/blob/main/README.md#technical-faq for more information',
-      default: '',
-      allowedPattern: '(^$|^.*@.*\\..*$)',
-    });
+    // const notificationsMail = new CfnParameter(this, 'NotificationsMail', {
+    //   type: 'String',
+    //   description:
+    //     'Mail address used for notifications. Please see https://github.com/superwerker/superwerker/blob/main/README.md#technical-faq for more information',
+    //   default: '',
+    //   allowedPattern: '(^$|^.*@.*\\..*$)',
+    // });
 
-    const includeBudget = new CfnParameter(this, 'IncludeBudget', {
-      type: 'String',
-      description: 'Enable AWS Budgets alarm for monthly AWS spending',
-      allowedValues: ['Yes', 'No'],
-      default: 'Yes',
-    });
+    // const includeBudget = new CfnParameter(this, 'IncludeBudget', {
+    //   type: 'String',
+    //   description: 'Enable AWS Budgets alarm for monthly AWS spending',
+    //   allowedValues: ['Yes', 'No'],
+    //   default: 'Yes',
+    // });
 
-    const includeGuardDuty = new CfnParameter(this, 'IncludeGuardDuty', {
-      type: 'String',
-      description: 'Enable Amazon GuardDuty',
-      allowedValues: ['Yes', 'No'],
-      default: 'Yes',
-    });
+    // const includeGuardDuty = new CfnParameter(this, 'IncludeGuardDuty', {
+    //   type: 'String',
+    //   description: 'Enable Amazon GuardDuty',
+    //   allowedValues: ['Yes', 'No'],
+    //   default: 'Yes',
+    // });
 
-    const includeSecurityHub = new CfnParameter(this, 'IncludeSecurityHub', {
-      type: 'String',
-      description: 'Enable AWS Security Hub',
-      allowedValues: ['Yes', 'No'],
-      default: 'Yes',
-    });
+    // const includeSecurityHub = new CfnParameter(this, 'IncludeSecurityHub', {
+    //   type: 'String',
+    //   description: 'Enable AWS Security Hub',
+    //   allowedValues: ['Yes', 'No'],
+    //   default: 'Yes',
+    // });
 
-    const includeBackup = new CfnParameter(this, 'IncludeBackup', {
-      type: 'String',
-      description: 'Enable automated backups',
-      allowedValues: ['Yes', 'No'],
-      default: 'Yes',
-    });
+    // const includeBackup = new CfnParameter(this, 'IncludeBackup', {
+    //   type: 'String',
+    //   description: 'Enable automated backups',
+    //   allowedValues: ['Yes', 'No'],
+    //   default: 'Yes',
+    // });
 
-    const includeServiceControlPolicies = new CfnParameter(this, 'IncludeServiceControlPolicies', {
-      type: 'String',
-      description: 'Enable service control policies in AWS organizations',
-      allowedValues: ['Yes', 'No'],
-      default: 'Yes',
-    });
+    // const includeServiceControlPolicies = new CfnParameter(this, 'IncludeServiceControlPolicies', {
+    //   type: 'String',
+    //   description: 'Enable service control policies in AWS organizations',
+    //   allowedValues: ['Yes', 'No'],
+    //   default: 'Yes',
+    // });
 
     /**
      * Core Components
      */
-    const emailAudit = new GenerateEmailAddress(this, 'GeneratedAuditAWSAccountEmail', {
-      domain: `${subdomain.value}.${domain.value}`,
-      name: `${SuperwerkerStack.AUDIT_ACCOUNT}`,
-    });
+    // const emailAudit = new GenerateEmailAddress(this, 'GeneratedAuditAWSAccountEmail', {
+    //   domain: `${subdomain.value}.${domain.value}`,
+    //   name: `${SuperwerkerStack.AUDIT_ACCOUNT}`,
+    // });
 
-    const emailLogArchive = new GenerateEmailAddress(this, 'GeneratedLogArchiveAWSAccountEmail', {
-      domain: `${subdomain.value}.${domain.value}`,
-      name: `${SuperwerkerStack.LOG_ARCHIVE_ACCOUNT}`,
-    });
+    // const emailLogArchive = new GenerateEmailAddress(this, 'GeneratedLogArchiveAWSAccountEmail', {
+    //   domain: `${subdomain.value}.${domain.value}`,
+    //   name: `${SuperwerkerStack.LOG_ARCHIVE_ACCOUNT}`,
+    // });
 
     // Prepare
-    const prepareStack = new Stack(this, 'Prepare', {
+    const prepareStack = new PrepareStack(this, 'PrepareAccount', {
       description: 'Prepares the AWS account for Superwerker.',
     });
-    (prepareStack.node.defaultChild as CfnStack).overrideLogicalId('Prepare');
+    (prepareStack.node.defaultChild as CfnStack).overrideLogicalId('PrepareAccount');
 
     // RootMail
-    const rootMailStack = new RootmailStack(this, 'RootMail', {
-      parameters: {
-        Domain: domain.value.toString(),
-        Subdomain: subdomain.value.toString(),
-      },
-    });
-    (rootMailStack.node.defaultChild as CfnStack).overrideLogicalId('RootMail');
+    // const rootMailStack = new RootmailStack(this, 'RootMail', {
+    //   parameters: {
+    //     Domain: domain.value.toString(),
+    //     Subdomain: subdomain.value.toString(),
+    //   },
+    // });
+    // (rootMailStack.node.defaultChild as CfnStack).overrideLogicalId('RootMail');
 
     // ControlTower
     const controlTowerStack = new ControlTowerStack(this, 'ControlTower', {
       parameters: {
-        AuditAWSAccountEmail: emailAudit.email,
-        LogArchiveAWSAccountEmail: emailLogArchive.email,
+        AuditAWSAccountEmail: 'mail1@standalone-04.superwerker.aws.ffm.kreuzwerker.de',
+        LogArchiveAWSAccountEmail: 'mail2@standalone-04.superwerker.aws.ffm.kreuzwerker.de',
       },
       description: 'Sets up the landing zone with control tower.',
     });
     (controlTowerStack.node.defaultChild as CfnStack).overrideLogicalId('ControlTower');
     controlTowerStack.addDependency(prepareStack);
 
-    // LivingDocumentation
-    const livingDocumentationStack = new LivingDocumentationStack(this, 'LivingDocumentation', {
-      parameters: {
-        SuperwerkerDomain: `${subdomain.value.toString()}.${domain.value.toString()}`,
-      },
-    });
-    (livingDocumentationStack.node.defaultChild as CfnStack).overrideLogicalId('LivingDocumentation');
+    //   // LivingDocumentation
+    //   const livingDocumentationStack = new LivingDocumentationStack(this, 'LivingDocumentation', {
+    //     parameters: {
+    //       SuperwerkerDomain: `${subdomain.value.toString()}.${domain.value.toString()}`,
+    //     },
+    //   });
+    //   (livingDocumentationStack.node.defaultChild as CfnStack).overrideLogicalId('LivingDocumentation');
 
-    /**
-     * optional components
-     */
+    //   /**
+    //    * optional components
+    //    */
 
-    // Backup
-    const backupCondition = new CfnCondition(this, 'IncludeBackupCondition', {
-      expression: Fn.conditionEquals(includeBackup, 'Yes'),
-    });
-    backupCondition.overrideLogicalId('IncludeBackup');
-    const backupStack = new BackupStack(this, 'Backup', {});
-    backupStack.addDependency(controlTowerStack);
-    (backupStack.node.defaultChild as CfnStack).overrideLogicalId('Backup');
-    (backupStack.node.defaultChild as CfnStack).cfnOptions.condition = backupCondition;
+    //   // Backup
+    //   const backupCondition = new CfnCondition(this, 'IncludeBackupCondition', {
+    //     expression: Fn.conditionEquals(includeBackup, 'Yes'),
+    //   });
+    //   backupCondition.overrideLogicalId('IncludeBackup');
+    //   const backupStack = new BackupStack(this, 'Backup', {});
+    //   backupStack.addDependency(controlTowerStack);
+    //   (backupStack.node.defaultChild as CfnStack).overrideLogicalId('Backup');
+    //   (backupStack.node.defaultChild as CfnStack).cfnOptions.condition = backupCondition;
 
-    // Budgets
-    const budgetCondition = new CfnCondition(this, 'IncludeBudgetCondition', {
-      expression: Fn.conditionEquals(includeBudget, 'Yes'),
-    });
-    budgetCondition.overrideLogicalId('IncludeBudget');
-    const budgetStack = new BudgetStack(this, 'Budget', {});
-    (budgetStack.node.defaultChild as CfnStack).overrideLogicalId('Budget');
-    (budgetStack.node.defaultChild as CfnStack).cfnOptions.condition = budgetCondition;
+    //   // Budgets
+    //   const budgetCondition = new CfnCondition(this, 'IncludeBudgetCondition', {
+    //     expression: Fn.conditionEquals(includeBudget, 'Yes'),
+    //   });
+    //   budgetCondition.overrideLogicalId('IncludeBudget');
+    //   const budgetStack = new BudgetStack(this, 'Budget', {});
+    //   (budgetStack.node.defaultChild as CfnStack).overrideLogicalId('Budget');
+    //   (budgetStack.node.defaultChild as CfnStack).cfnOptions.condition = budgetCondition;
 
-    // GuardDuty
-    const guardDutyCondition = new CfnCondition(this, 'IncludeGuardDutyCondition', {
-      expression: Fn.conditionEquals(includeGuardDuty, 'Yes'),
-    });
-    guardDutyCondition.overrideLogicalId('IncludeGuardDuty');
-    const guardDutyStack = new GuardDutyStack(this, 'GuardDuty', {});
-    (guardDutyStack.node.defaultChild as CfnStack).overrideLogicalId('GuardDuty');
-    (guardDutyStack.node.defaultChild as CfnStack).cfnOptions.condition = guardDutyCondition;
+    //   // GuardDuty
+    //   const guardDutyCondition = new CfnCondition(this, 'IncludeGuardDutyCondition', {
+    //     expression: Fn.conditionEquals(includeGuardDuty, 'Yes'),
+    //   });
+    //   guardDutyCondition.overrideLogicalId('IncludeGuardDuty');
+    //   const guardDutyStack = new GuardDutyStack(this, 'GuardDuty', {});
+    //   (guardDutyStack.node.defaultChild as CfnStack).overrideLogicalId('GuardDuty');
+    //   (guardDutyStack.node.defaultChild as CfnStack).cfnOptions.condition = guardDutyCondition;
 
-    // Notifications
-    const notificationsCondition = new CfnCondition(this, 'IncludeNotificationsCondition', {
-      expression: Fn.conditionNot(Fn.conditionEquals(notificationsMail, '')),
-    });
-    notificationsCondition.overrideLogicalId('IncludeNotifications');
-    const notificationsStack = new NotificationsStack(this, 'Notifications', {
-      parameters: {
-        NotificationsMail: notificationsMail.value.toString(),
-      },
-    });
-    notificationsStack.addDependency(rootMailStack);
-    (notificationsStack.node.defaultChild as CfnStack).overrideLogicalId('Notifications');
-    (notificationsStack.node.defaultChild as CfnStack).cfnOptions.condition = notificationsCondition;
+    //   // Notifications
+    //   const notificationsCondition = new CfnCondition(this, 'IncludeNotificationsCondition', {
+    //     expression: Fn.conditionNot(Fn.conditionEquals(notificationsMail, '')),
+    //   });
+    //   notificationsCondition.overrideLogicalId('IncludeNotifications');
+    //   const notificationsStack = new NotificationsStack(this, 'Notifications', {
+    //     parameters: {
+    //       NotificationsMail: notificationsMail.value.toString(),
+    //     },
+    //   });
+    //   notificationsStack.addDependency(rootMailStack);
+    //   (notificationsStack.node.defaultChild as CfnStack).overrideLogicalId('Notifications');
+    //   (notificationsStack.node.defaultChild as CfnStack).cfnOptions.condition = notificationsCondition;
 
-    // SecurityHub
-    const securityHubCondition = new CfnCondition(this, 'IncludeSecurityHubCondition', {
-      expression: Fn.conditionEquals(includeSecurityHub, 'Yes'),
-    });
-    securityHubCondition.overrideLogicalId('IncludeSecurityHub');
-    const securityHubStack = new SecurityHubStack(this, 'SecurityHub', {});
-    (securityHubStack.node.defaultChild as CfnStack).overrideLogicalId('SecurityHub');
-    (securityHubStack.node.defaultChild as CfnStack).cfnOptions.condition = securityHubCondition;
+    //   // SecurityHub
+    //   const securityHubCondition = new CfnCondition(this, 'IncludeSecurityHubCondition', {
+    //     expression: Fn.conditionEquals(includeSecurityHub, 'Yes'),
+    //   });
+    //   securityHubCondition.overrideLogicalId('IncludeSecurityHub');
+    //   const securityHubStack = new SecurityHubStack(this, 'SecurityHub', {});
+    //   (securityHubStack.node.defaultChild as CfnStack).overrideLogicalId('SecurityHub');
+    //   (securityHubStack.node.defaultChild as CfnStack).cfnOptions.condition = securityHubCondition;
 
-    // ServiceControlPolicies
-    const serviceControlPoliciesCondition = new CfnCondition(this, 'IncludeServiceControlPoliciesCondition', {
-      expression: Fn.conditionEquals(includeServiceControlPolicies, 'Yes'),
-    });
-    serviceControlPoliciesCondition.overrideLogicalId('IncludeServiceControlPolicies');
-    const serviceControlPoliciesStack = new ServiceControlPoliciesStack(this, 'ServiceControlPolicies', {
-      parameters: {
-        IncludeSecurityHub: `${Fn.conditionIf('IncludeSecurityHub', 'true', 'false')}`,
-        IncludeBackup: `${Fn.conditionIf('IncludeBackup', 'true', 'false')}`,
-      },
-    });
-    serviceControlPoliciesStack.addDependency(controlTowerStack);
-    (serviceControlPoliciesStack.node.defaultChild as CfnStack).overrideLogicalId('ServiceControlPolicies');
-    (serviceControlPoliciesStack.node.defaultChild as CfnStack).cfnOptions.condition = serviceControlPoliciesCondition;
+    //   // ServiceControlPolicies
+    //   const serviceControlPoliciesCondition = new CfnCondition(this, 'IncludeServiceControlPoliciesCondition', {
+    //     expression: Fn.conditionEquals(includeServiceControlPolicies, 'Yes'),
+    //   });
+    //   serviceControlPoliciesCondition.overrideLogicalId('IncludeServiceControlPolicies');
+    //   const serviceControlPoliciesStack = new ServiceControlPoliciesStack(this, 'ServiceControlPolicies', {
+    //     parameters: {
+    //       IncludeSecurityHub: `${Fn.conditionIf('IncludeSecurityHub', 'true', 'false')}`,
+    //       IncludeBackup: `${Fn.conditionIf('IncludeBackup', 'true', 'false')}`,
+    //     },
+    //   });
+    //   serviceControlPoliciesStack.addDependency(controlTowerStack);
+    //   (serviceControlPoliciesStack.node.defaultChild as CfnStack).overrideLogicalId('ServiceControlPolicies');
+    //   (serviceControlPoliciesStack.node.defaultChild as CfnStack).cfnOptions.condition = serviceControlPoliciesCondition;
   }
 }
