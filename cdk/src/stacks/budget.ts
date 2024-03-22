cd521dc2825d51c3dc23b0e6fc0efb69be1d97ce
Peer Mueller <peer.mueller@kreuzwerker.de>
2024-03-22 12:25:08 +0100

    fix: fix logical ids of resources adapt test to check logical ids

--- cdk/src/stacks/budget.ts
+++ cdk/src/stacks/budget.ts
@@ -7,6 +7,8 @@ import {
   aws_cloudwatch_actions as actions,
   aws_budgets as budgets,
 } from 'aws-cdk-lib';
+import { CfnBudget } from 'aws-cdk-lib/aws-budgets';
+import { CfnTopic } from 'aws-cdk-lib/aws-sns';
 import { Construct } from 'constructs';
 
 export class BudgetStack extends NestedStack {
@@ -14,8 +16,24 @@ export class BudgetStack extends NestedStack {
     super(scope, id, props);
 
     const budgetNotificationTopic = new sns.Topic(this, 'BudgetNotification');
+    (budgetNotificationTopic.node.defaultChild as CfnTopic).overrideLogicalId('BudgetNotification');
 
     budgetNotificationTopic.grantPublish(new iam.ServicePrincipal('budget.amazonaws.com'));
+
+    const snsTopicPolicy = new sns.TopicPolicy(this, 'BudgetNotificationPolicy', {
+      policyDocument: new iam.PolicyDocument({
+        statements: [
+          new iam.PolicyStatement({
+            actions: ['sns:Publish'],
+            effect: iam.Effect.ALLOW,
+            principals: [new iam.ServicePrincipal('budget.amazonaws.com')],
+            resources: [budgetNotificationTopic.topicArn],
+          }),
+        ],
+      }),
+      topics: [budgetNotificationTopic],
+    });
+    (snsTopicPolicy.node.defaultChild as CfnTopic).overrideLogicalId('BudgetNotificationPolicy');
 
     const budgetAlarm = new cw.Alarm(this, 'BudgetAlarm', {
       comparisonOperator: cw.ComparisonOperator.GREATER_THAN_THRESHOLD,
@@ -32,6 +50,7 @@ export class BudgetStack extends NestedStack {
         statistic: cw.Stats.SUM,
       }),
     });
+    (budgetAlarm.node.defaultChild as CfnBudget).overrideLogicalId('BudgetAlarm');
 
     budgetAlarm.addAlarmAction(new actions.SsmAction(actions.OpsItemSeverity.MEDIUM, actions.OpsItemCategory.COST));
 
