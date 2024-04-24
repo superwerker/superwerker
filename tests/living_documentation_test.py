94e94e89f5f1178de2ef5ed11db39c98a532c332
Peer Mueller <peer.mueller@kreuzwerker.de>
2024-04-24 15:24:18 +0200

    test: check superwerker dashboard content

--- tests/living_documentation_test.py
+++ tests/living_documentation_test.py
@@ -1,32 +1,59 @@
 import boto3
 import pytest
 import botocore
+import json
 
 cw = boto3.client('cloudwatch')
 lambda_client = boto3.client('lambda')
 
-
-def test_superwerker_dashboard():
-    superwerker_dashboard = cw.get_dashboard(DashboardName="Superwerker-LivingDocumentation")
-    assert superwerker_dashboard, "No superwerker dashboard found"
-
+@pytest.fixture(scope="module")
+def dashboard_generator_function():
     response_iterator = lambda_client.get_paginator('list_functions').paginate()
-    
-    function_exists = False
     for page in response_iterator:
         for function in page['Functions']:
             if function['FunctionName'].startswith('superwerker-LivingDocumen-'):
-                function_exists = True
-    assert function_exists, "superwerker dashboard generator lambda function not found"
-    
-def test_superwerker_legacy_dashboard():
-    paginator = lambda_client.get_paginator('list_functions')
-    page_iterator = paginator.paginate()
+                return function
 
-    for page in page_iterator:
-        for function in page['Functions']:
-            if function['FunctionName'].startswith('superwerker-LivingDocumen-DashboardGenerator'):
-                lambda_client.invoke(FunctionName=function['FunctionName'], InvocationType='RequestResponse')
+
+def test_superwerker_dashboard(dashboard_generator_function):
+    superwerker_dashboard = cw.get_dashboard(DashboardName="Superwerker-LivingDocumentation")
+    assert superwerker_dashboard, "No superwerker dashboard found"
+    
+    assert dashboard_generator_function, "superwerker dashboard generator lambda function not found"
+
+    assert json.loads(superwerker_dashboard["DashboardBody"]) == json.loads('''{{
+    "widgets": [
+        {{
+            "height": 17,
+            "width": 20,
+            "y": 0,
+            "x": 0,
+            "type": "custom",
+            "properties": {{
+                "endpoint": "{0}",
+                "title": "",
+                "updateOn": {{
+                    "refresh": true,
+                    "resize": true,
+                    "timeRange": true
+                }}
+            }}
+        }}
+    ]
+}}'''.format(dashboard_generator_function['FunctionArn'])), 'superwerker dashboard body does not match expected value'
+    
+def test_superwerker_dashboard_contents(dashboard_generator_function):
+    response = lambda_client.invoke(FunctionName=dashboard_generator_function['FunctionName'], InvocationType='RequestResponse')
+
+    assert response['StatusCode'] == 200, 'Lambda invocation failed'
+    lambda_payload = str(response['Payload'].read())
+    assert 'DNS configuration is set up correctly' in lambda_payload, 'Lambda response should contain "DNS configuration is set up correctly"'
+    assert 'https://github.com/superwerker/superwerker' in lambda_payload, 'Lambda response should contain "link to superwerker GitHub repository"'
+    assert 'Next steps' in lambda_payload, 'Lambda response should contain "Next steps"'
+
+
+def test_superwerker_legacy_dashboard_does_not_exist(dashboard_generator_function):
+    lambda_client.invoke(FunctionName=dashboard_generator_function['FunctionName'], InvocationType='RequestResponse')
 
     with pytest.raises(botocore.exceptions.ClientError) as exception:
         cw.get_dashboard(DashboardName="superwerker")
