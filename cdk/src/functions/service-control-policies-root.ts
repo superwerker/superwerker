56ce4253101350269f5c1b27d8de925f93c86679
gauravdubey <gaurav.dubey@kreuzwerker.de>
2024-04-26 11:04:19 +0200

    feat: migrate scp stack to cdk

---
+++ cdk/src/functions/service-control-policies-root.ts
@@ -0,0 +1,93 @@
+import {
+  AttachPolicyCommand,
+  CreatePolicyCommand,
+  DeletePolicyCommand,
+  DetachPolicyCommand,
+  ListRootsCommand,
+  OrganizationsClient,
+  PolicyType,
+  UpdatePolicyCommand,
+} from '@aws-sdk/client-organizations';
+import { CdkCustomResourceEvent, CdkCustomResourceResponse, Context } from 'aws-lambda';
+
+async function getRootId(organizationClient: OrganizationsClient): Promise<string | undefined> {
+  let id = '';
+  try {
+    const command = new ListRootsCommand({});
+    const response = await organizationClient.send(command);
+
+    if (!response.Roots || response.Roots.length === 0) {
+      console.warn('No roots found in the organization');
+      return 'No roots';
+    }
+
+    const root = response.Roots[0];
+    id = root.Id || '';
+    return id;
+  } catch (error) {
+    console.error(`Error getting root accounts for ${id}`, error);
+    return `Error: ${error}`;
+  }
+}
+
+export async function handler(event: CdkCustomResourceEvent, _context: Context): Promise<CdkCustomResourceResponse> {
+  let client = new OrganizationsClient({ region: 'us-east-1' });
+
+  switch (event.RequestType) {
+    case 'Create':
+      console.log('Creating Policy for : ', event.LogicalResourceId);
+      try {
+        let rootId = await getRootId(client);
+
+        const commandCreatePolicyRoot = new CreatePolicyCommand({
+          Type: PolicyType.SERVICE_CONTROL_POLICY,
+          Description: `superwerker - ${event.LogicalResourceId}`,
+          Name: event.ResourceProperties.scpName,
+          Content: event.ResourceProperties.policy,
+        });
+
+        const responseCreatePolicyRoot = await client.send(commandCreatePolicyRoot);
+
+        const commandAttachPolicyRoot = new AttachPolicyCommand({
+          PolicyId: responseCreatePolicyRoot.Policy?.PolicySummary?.Id || '',
+          TargetId: rootId,
+        });
+
+        await client.send(commandAttachPolicyRoot);
+
+        return { SUCESS: 'SCPs have been successfully created for Root account' };
+      } catch (e) {
+        console.log('Error during Creating Policy: ', e);
+        return { ErrorMessage: `Error during creating policy: ${e}` };
+      }
+
+    case 'Update':
+      console.log('Updating Policy: ', event.LogicalResourceId);
+      const commandUpdatePolicy = new UpdatePolicyCommand({
+        PolicyId: event.PhysicalResourceId,
+        Description: `superwerker - ${event.LogicalResourceId}`,
+        Name: event.ResourceProperties.scpName,
+        Content: event.ResourceProperties.policy,
+      });
+
+      const responseUpdatePolicy = await client.send(commandUpdatePolicy);
+      return responseUpdatePolicy;
+
+    case 'Delete':
+      console.log('Deleting Policy: ', event.LogicalResourceId);
+
+      const commandDetachPolicy = new DetachPolicyCommand({
+        PolicyId: event.PhysicalResourceId,
+        TargetId: await getRootId(client),
+      });
+
+      await client.send(commandDetachPolicy);
+
+      const commandDeletePolicy = new DeletePolicyCommand({
+        PolicyId: event.PhysicalResourceId,
+      });
+
+      const responseDeletePolicy = await client.send(commandDeletePolicy);
+      return responseDeletePolicy;
+  }
+}
---
+++ cdk/src/functions/service-control-policies-sandbox.ts
@@ -0,0 +1,121 @@
+import {
+  AttachPolicyCommand,
+  CreatePolicyCommand,
+  DeletePolicyCommand,
+  DetachPolicyCommand,
+  ListAccountsForParentCommand,
+  ListRootsCommand,
+  OrganizationsClient,
+  PolicyType,
+  UpdatePolicyCommand,
+} from '@aws-sdk/client-organizations';
+import { CdkCustomResourceEvent, CdkCustomResourceResponse, Context } from 'aws-lambda';
+
+async function getRootId(organizationClient: OrganizationsClient): Promise<string | undefined> {
+  let id = '';
+  try {
+    const command = new ListRootsCommand({});
+    const response = await organizationClient.send(command);
+
+    if (!response.Roots || response.Roots.length === 0) {
+      console.warn('No roots found in the organization');
+      return 'No roots';
+    }
+
+    const root = response.Roots[0];
+    id = root.Id || '';
+    return id;
+  } catch (error) {
+    console.error(`Error getting root accounts for ${id}`, error);
+    return `Error: ${error}`;
+  }
+}
+
+async function getSandboxId(organizationClient: OrganizationsClient): Promise<string | undefined> {
+  let root = await getRootId(organizationClient);
+
+  try {
+    const command = new ListAccountsForParentCommand({ ParentId: root });
+    const response = await organizationClient.send(command);
+
+    const accounts = response.Accounts || [];
+
+    for (const account of accounts) {
+      if (account.Name == 'Sandbox') {
+        return account.Id;
+      }
+    }
+    return '';
+  } catch (e) {
+    return '';
+  }
+
+  return '';
+}
+
+export async function handler(event: CdkCustomResourceEvent, _context: Context): Promise<CdkCustomResourceResponse> {
+  let client = new OrganizationsClient({ region: 'us-east-1' });
+
+  switch (event.RequestType) {
+    case 'Create':
+      console.log('Creating Policy for : ', event.LogicalResourceId);
+      try {
+        //Attach SCP to Sandbox Account
+        let sandboxId = await getSandboxId(client);
+
+        if (sandboxId?.includes('Error') || sandboxId?.includes('No root')) {
+          return { Error: sandboxId };
+        }
+
+        const commandCreatePolicySandbox = new CreatePolicyCommand({
+          Type: PolicyType.SERVICE_CONTROL_POLICY,
+          Description: `superwerker - sandbox - ${event.LogicalResourceId}`,
+          Name: event.ResourceProperties.scpNameRoot,
+          Content: event.ResourceProperties.policySandbox,
+        });
+
+        const responseCreatePolicySandbox = await client.send(commandCreatePolicySandbox);
+
+        const commandAttachPolicySandbox = new AttachPolicyCommand({
+          PolicyId: responseCreatePolicySandbox.Policy?.PolicySummary?.Id || '',
+          TargetId: sandboxId,
+        });
+
+        await client.send(commandAttachPolicySandbox);
+
+        return { SUCESS: 'SCPs have been successfully created for Sandbox account' };
+      } catch (e) {
+        console.log('Error during Creating Policy for Sandbox account: ', e);
+        return { ErrorMessage: `Error during creating policy for Sandbox account: ${e}` };
+      }
+
+    case 'Update':
+      console.log('Updating Policy: ', event.LogicalResourceId);
+      const commandUpdatePolicy = new UpdatePolicyCommand({
+        PolicyId: event.PhysicalResourceId,
+        Description: `superwerker - ${event.LogicalResourceId}`,
+        Name: event.ResourceProperties.scpName,
+        Content: event.ResourceProperties.policy,
+      });
+
+      const responseUpdatePolicy = await client.send(commandUpdatePolicy);
+      return responseUpdatePolicy;
+
+    case 'Delete':
+      console.log('Deleting Policy: ', event.LogicalResourceId);
+
+      const commandDetachPolicy = new DetachPolicyCommand({
+        PolicyId: event.PhysicalResourceId,
+        TargetId: await getRootId(client),
+      });
+
+      await client.send(commandDetachPolicy);
+
+      const commandDeletePolicy = new DeletePolicyCommand({
+        PolicyId: event.PhysicalResourceId,
+      });
+
+      const responseDeletePolicy = await client.send(commandDeletePolicy);
+      return responseDeletePolicy;
+  }
+}
