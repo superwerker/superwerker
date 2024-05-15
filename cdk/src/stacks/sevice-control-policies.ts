bbbce020c3aa4bd595e2ab59ae51c272df402865
gauravdubey <gaurav.dubey@kreuzwerker.de>
2024-05-15 13:44:33 +0200

    build: update scp lambda

--- cdk/src/stacks/sevice-control-policies.ts
+++ cdk/src/stacks/sevice-control-policies.ts
@@ -20,7 +20,7 @@ export class ServiceControlPoliciesStack extends NestedStack {
         },
       },
       actions: [
-        'iam:AttachRolePolicy',
+        // 'iam:AttachRolePolicy',
         'iam:CreateRole',
         'iam:DeleteRole',
         'iam:DeleteRolePermissionsBoundary',
