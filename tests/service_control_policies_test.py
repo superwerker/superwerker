79cc5f2765a218db5980247c033d25976489dfb9
gauravdubey <gaurav.dubey@kreuzwerker.de>
2024-05-15 11:45:23 +0200

    build: update scp lambda

--- tests/service_control_policies_test.py
+++ tests/service_control_policies_test.py
@@ -20,35 +20,35 @@ def test_superwerker_service_control_policies():
             superwerker_policy_id = scp['Id']
             superwerker_policy=organizations.describe_policy(PolicyId=superwerker_policy_id)
     assert superwerker_policy['Policy']['PolicySummary']['Description'] == 'superwerker - SCPBaseline'
-    # assert ''.join(superwerker_policy['Policy']['Content'].split()) == ''.join('''{
-    # "Version": "2012-10-17",
-    # "Statement": [
-    #     {
-    #     "Condition": {
-    #         "ArnNotLike": {
-    #         "aws:PrincipalARN": "arn:aws:iam::*:role/stacksets-exec-*"
-    #         }
-    #     },
-    #     "Action": [
-    #         'iam:AttachRolePolicy',
-    #         'iam:CreateRole',
-    #         'iam:DeleteRole',
-    #         'iam:DeleteRolePermissionsBoundary',
-    #         'iam:DeleteRolePolicy',
-    #         'iam:DetachRolePolicy',
-    #         'iam:PutRolePermissionsBoundary',
-    #         'iam:PutRolePolicy',
-    #         'iam:UpdateAssumeRolePolicy',
-    #         'iam:UpdateRole',
-    #         'iam:UpdateRoleDescription',
-    #     ],
-    #     "Resource": [
-    #         "arn:aws:iam::*:role/service-role/AWSBackupDefaultServiceRole",
-    #         "arn:aws:iam::*:role/SuperwerkerBackupTagsEnforcementRemediationRole"
-    #     ],
-    #     "Effect": "Deny",
-    #     "Sid": "SWProtectBackup"
-    #     }
-    # ]
-    # }'''.split()), 'Policy content does not match expected content'
+    assert ''.join(superwerker_policy['Policy']['Content'].split()) == ''.join('''{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+        "Condition": {
+            "ArnNotLike": {
+            "aws:PrincipalARN": "arn:aws:iam::*:role/stacksets-exec-*"
+            }
+        },
+        "Action": [
+            'iam:AttachRolePolicy',
+            'iam:CreateRole',
+            'iam:DeleteRole',
+            'iam:DeleteRolePermissionsBoundary',
+            'iam:DeleteRolePolicy',
+            'iam:DetachRolePolicy',
+            'iam:PutRolePermissionsBoundary',
+            'iam:PutRolePolicy',
+            'iam:UpdateAssumeRolePolicy',
+            'iam:UpdateRole',
+            'iam:UpdateRoleDescription',
+        ],
+        "Resource": [
+            "arn:aws:iam::*:role/service-role/AWSBackupDefaultServiceRole",
+            "arn:aws:iam::*:role/SuperwerkerBackupTagsEnforcementRemediationRole"
+        ],
+        "Effect": "Deny",
+        "Sid": "SWProtectBackup"
+        }
+    ]
+    }'''.split()), 'Policy content does not match expected content'
     
