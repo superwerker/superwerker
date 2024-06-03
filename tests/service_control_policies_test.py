cef1a02ef7bc23f2292fac54d9334c4b6fc17a62
gauravdubey <gaurav.dubey@kreuzwerker.de>
2024-06-03 12:02:40 +0200

    build: updated scp test q

--- tests/service_control_policies_test.py
+++ tests/service_control_policies_test.py
@@ -22,39 +22,39 @@ def test_superwerker_service_control_policies():
             assert superwerker_policy['Policy']['PolicySummary']['Description'] == 'superwerker - SCPRoot'
 
             expectedPolicyJson = '''{
-            'Version': '2012-10-17',
-            'Statement': [
+            "Version": '2012-10-17',
+            "Statement": [
                 {
-                    'Action': 'organizations:LeaveOrganization',
-                    'Effect': 'Deny',
-                    'Resource': '*',
-                    'Sid': 'PreventLeavingOrganization'
+                    "Action": "organizations:LeaveOrganization",
+                    "Effect": "Deny",
+                    "Resource": "*",
+                    "Sid": "PreventLeavingOrganization"
                 },
                 {
-                    'Condition': {
-                        'ArnNotLike': {
-                        'aws:PrincipalARN': 'arn:aws:iam::*:role/stacksets-exec-*'
+                    "Condition": {
+                        "ArnNotLike": {
+                        "aws:PrincipalARN": "arn:aws:iam::*:role/stacksets-exec-*"
                         }
                     },
-                    'Action': [
-                        'iam:AttachRolePolicy',
-                        'iam:CreateRole',
-                        'iam:DeleteRole',
-                        'iam:DeleteRolePermissionsBoundary',
-                        'iam:DeleteRolePolicy',
-                        'iam:DetachRolePolicy',
-                        'iam:PutRolePermissionsBoundary',
-                        'iam:PutRolePolicy',
-                        'iam:UpdateAssumeRolePolicy',
-                        'iam:UpdateRole',
-                        'iam:UpdateRoleDescription'
+                    "Action": [
+                        "iam:AttachRolePolicy",
+                        "iam:CreateRole",
+                        "iam:DeleteRole",
+                        "iam:DeleteRolePermissionsBoundary",
+                        "iam:DeleteRolePolicy",
+                        "iam:DetachRolePolicy",
+                        "iam:PutRolePermissionsBoundary",
+                        "iam:PutRolePolicy",
+                        "iam:UpdateAssumeRolePolicy",
+                        "iam:UpdateRole",
+                        "iam:UpdateRoleDescription"
                     ],
-                    'Resource': [
-                        'arn:aws:iam::*:role/service-role/AWSBackupDefaultServiceRole',
-                        'arn:aws:iam::*:role/SuperwerkerBackupTagsEnforcementRemediationRole'
+                    "Resource": [
+                        "arn:aws:iam::*:role/service-role/AWSBackupDefaultServiceRole",
+                        "arn:aws:iam::*:role/SuperwerkerBackupTagsEnforcementRemediationRole"
                     ],
-                    'Effect': 'Deny',
-                    'Sid': 'SWProtectBackup'
+                    "Effect": "Deny",
+                    "Sid": "SWProtectBackup"
                 }
             ]
             }'''
@@ -69,43 +69,43 @@ def test_superwerker_service_control_policies():
         elif scp['Name'] == 'superwerker-sandbox':
             superwerker_policy_id = scp['Id']
             superwerker_policy=organizations.describe_policy(PolicyId=superwerker_policy_id)
-            assert superwerker_policy['Policy']['PolicySummary']['Description'] == 'superwerker - SCPBaseline'
+            assert superwerker_policy['Policy']['PolicySummary']['Description'] == 'superwerker - sandbox - SCPSandbox'
 
             expectedPolicyJson = '''{
-                'Sid': 'DenyExpensiveResourceCreation',
-                'Effect': 'Deny',
-                'Action': [
-                    'route53domains:RegisterDomain',
-                    'route53domains:RenewDomain',
-                    'route53domains:TransferDomain',
-                    'ec2:ModifyReservedInstances',
-                    'ec2:PurchaseHostReservation',
-                    'ec2:PurchaseReservedInstancesOffering',
-                    'ec2:PurchaseScheduledInstances',
-                    'rds:PurchaseReservedDBInstancesOffering',
-                    'dynamodb:PurchaseReservedCapacityOfferings',
-                    's3:PutObjectRetention',
-                    's3:PutObjectLegalHold',
-                    's3:BypassGovernanceRetention',
-                    's3:PutBucketObjectLockConfiguration',
-                    'elasticache:PurchaseReservedCacheNodesOffering',
-                    'redshift:PurchaseReservedNodeOffering',
-                    'savingsplans:CreateSavingsPlan',
-                    'aws-marketplace:AcceptAgreementApprovalRequest',
-                    'aws-marketplace:Subscribe',
-                    'shield:CreateSubscription',
-                    'acm-pca:CreateCertificateAuthority',
-                    'es:PurchaseReservedElasticsearchInstanceOffering',
-                    'outposts:CreateOutpost',
-                    'snowball:CreateCluster',
-                    's3-object-lambda:PutObjectLegalHold',
-                    's3-object-lambda:PutObjectRetention',
-                    'glacier:InitiateVaultLock',
-                    'glacier:CompleteVaultLock',
-                    'es:PurchaseReservedInstanceOffering',
-                    'backup:PutBackupVaultLockConfiguration'
+                "Sid": "DenyExpensiveResourceCreation",
+                "Effect": "Deny",
+                "Action": [
+                    "route53domains:RegisterDomain",
+                    "route53domains:RenewDomain",
+                    "route53domains:TransferDomain",
+                    "ec2:ModifyReservedInstances",
+                    "ec2:PurchaseHostReservation",
+                    "ec2:PurchaseReservedInstancesOffering",
+                    "ec2:PurchaseScheduledInstances",
+                    "rds:PurchaseReservedDBInstancesOffering",
+                    "dynamodb:PurchaseReservedCapacityOfferings",
+                    "s3:PutObjectRetention",
+                    "s3:PutObjectLegalHold",
+                    "s3:BypassGovernanceRetention",
+                    "s3:PutBucketObjectLockConfiguration",
+                    "elasticache:PurchaseReservedCacheNodesOffering",
+                    "redshift:PurchaseReservedNodeOffering",
+                    "savingsplans:CreateSavingsPlan",
+                    "aws-marketplace:AcceptAgreementApprovalRequest",
+                    "aws-marketplace:Subscribe",
+                    "shield:CreateSubscription",
+                    "acm-pca:CreateCertificateAuthority",
+                    "es:PurchaseReservedElasticsearchInstanceOffering",
+                    "outposts:CreateOutpost",
+                    "snowball:CreateCluster",
+                    "s3-object-lambda:PutObjectLegalHold",
+                    "s3-object-lambda:PutObjectRetention",
+                    "glacier:InitiateVaultLock",
+                    "glacier:CompleteVaultLock",
+                    "es:PurchaseReservedInstanceOffering",
+                    "backup:PutBackupVaultLockConfiguration"
                 ],
-                'Resource': '*'
+                "Resource": "*"
                 }'''
 
             expectedPolicyDict = json.loads(expectedPolicyJson)
