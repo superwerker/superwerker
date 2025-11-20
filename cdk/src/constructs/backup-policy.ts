a423089dbddb3f268b21201bf68fdd2b4cfce0b4
Torsten Dreyer <torsten@t3r.de>
2025-11-20 10:21:08 +0100

    fix: bump lambda runtime to python 3.14

--- cdk/src/constructs/backup-policy.ts
+++ cdk/src/constructs/backup-policy.ts
@@ -43,7 +43,7 @@ class BackupPolicyProvider extends Construct {
     const backupPolicyFn = new PythonFunction(this, 'backup-policy-on-event', {
       entry: path.join(__dirname, '..', 'functions', 'backup-policy'),
       handler: 'handler',
-      runtime: Runtime.PYTHON_3_9,
+      runtime: Runtime.PYTHON_3_14,
       timeout: Duration.seconds(200),
       initialPolicy: [
         new iam.PolicyStatement({
